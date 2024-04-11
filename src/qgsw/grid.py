039ea788fc885caa292ca13e2ab9079aedc2e7f9
Gaétan Rigaut <gaetanrigaut.s@gmail.com>
2024-04-11 16:28:15 +0200

    Implement all different grids

--- src/qgsw/grid.py
+++ src/qgsw/grid.py
@@ -13,7 +13,7 @@ class Grid:
     def __init__(self, config: GridConfig) -> None:
         """Instantiate the Grid."""
         self._config = config
-        self._generate_grid()
+        self._generate_grids()
 
     @property
     def nx(self) -> int:
@@ -36,36 +36,83 @@ class Grid:
         return self._config.ly
 
     @property
-    def x(self) -> torch.Tensor:
-        """X."""
-        return self._x
+    def omega_xy(self) -> tuple[torch.Tensor, torch.Tensor]:
+        """X,Y cordinates of the Omega grid ('classical' grid corners).
+
+        See https://agupubs.onlinelibrary.wiley.com/doi/epdf/10.1029/2021MS002663#JAME21507.indd%3Ahl_jame21507-fig-0001%3A73
+        for more details.
+        """
+        return self._omega_x, self._omega_y
 
     @property
-    def y(self) -> torch.Tensor:
-        """Y."""
-        return self._y
+    def h_xy(self) -> tuple[torch.Tensor, torch.Tensor]:
+        """X,Y coordinates of the H grid (center of 'classical' grid cells).
 
-    def _generate_grid(self) -> tuple[torch.Tensor, torch.Tensor]:
+        See https://agupubs.onlinelibrary.wiley.com/doi/epdf/10.1029/2021MS002663#JAME21507.indd%3Ahl_jame21507-fig-0001%3A73
+        for more details.
+        """
+        return self._h_x, self._h_y
+
+    @property
+    def u_xy(self) -> tuple[torch.Tensor, torch.Tensor]:
+        """X,Y coordinates of the u grid .
+
+        See https://agupubs.onlinelibrary.wiley.com/doi/epdf/10.1029/2021MS002663#JAME21507.indd%3Ahl_jame21507-fig-0001%3A73
+        for more details.
+        """
+        return self._u_x, self._u_y
+
+    @property
+    def v_xy(self) -> tuple[torch.Tensor, torch.Tensor]:
+        """X,Y coordinates of the H grid (center of 'classical' grid cells).
+
+        See https://agupubs.onlinelibrary.wiley.com/doi/epdf/10.1029/2021MS002663#JAME21507.indd%3Ahl_jame21507-fig-0001%3A73
+        for more details.
+        """
+        return self._v_x, self._v_y
+
+    def _generate_grids(self) -> tuple[torch.Tensor, torch.Tensor]:
         """Generate X,Y grids.
 
         Returns:
             tuple[torch.Tensor, torch.Tensor]: X,Y
         """
-        self._x, self._y = torch.meshgrid(
-            torch.linspace(
-                0,
-                self.lx,
-                self.nx + 1,
-                dtype=torch.float64,
-                device=DEVICE,
-            ),
-            torch.linspace(
-                0,
-                self.ly,
-                self.ny + 1,
-                dtype=torch.float64,
-                device=DEVICE,
-            ),
+        xs = torch.linspace(
+            self._config.x_min,
+            self._config.x_max,
+            self.nx + 1,
+            dtype=torch.float64,
+            device=DEVICE,
+        )
+        ys = torch.linspace(
+            self._config.y_min,
+            self._config.y_max,
+            self.nx + 1,
+            dtype=torch.float64,
+            device=DEVICE,
+        )
+        x_centers = 0.5 * (xs[1:] + xs[:-1])
+        y_centers = 0.5 * (ys[1:] + ys[:-1])
+
+        self._omega_x, self._omega_y = torch.meshgrid(
+            xs,
+            ys,
+            indexing="ij",
+        )
+
+        self._h_x, self._h_y = torch.meshgrid(
+            x_centers,
+            y_centers,
+            indexing="ij",
+        )
+        self._u_x, self._u_y = torch.meshgrid(
+            xs,
+            y_centers,
+            indexing="ij",
+        )
+        self._v_x, self._v_y = torch.meshgrid(
+            x_centers,
+            ys,
             indexing="ij",
         )
 
@@ -79,7 +126,7 @@ class Grid:
         Returns:
             torch.Tensor: _description_
         """
-        return f0 + beta * (self.y - self.ly / 2)
+        return f0 + beta * (self.omega_xy[1] - self.ly / 2)
 
     @classmethod
     def from_runconfig(cls, run_config: RunConfig) -> Self:
