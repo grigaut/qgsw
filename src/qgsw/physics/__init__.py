cdbc6ce7f16666c0bb3cb17593bb019656545efa
Gaétan Rigaut <gaetanrigaut.s@gmail.com>
2024-04-17 17:21:50 +0200

    Set dimensionless number at qgsw/physics level

--- src/qgsw/physics/__init__.py
+++ src/qgsw/physics/__init__.py
@@ -1,1 +1,9 @@
 """Physics-related computations."""
+
+from qgsw.physics.dimensionless_numbers import (
+    compute_burger,
+    compute_deformation_radius,
+    compute_rossby,
+)
+
+__all__ = ["compute_burger", "compute_deformation_radius", "compute_rossby"]
