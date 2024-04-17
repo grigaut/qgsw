dc39505b0e7cbe7156d6a0082916df4f2d6079ff
grigaut <70752330+grigaut@users.noreply.github.com>
2024-04-17 17:23:55 +0200

    Set dimensionless number at qgsw/physics level (#11)
    * Remove useless path appending
    
    * Implement dimensionless numbers computation.
    
    * Set dimensionless number at qgsw/physics level

---
+++ src/qgsw/physics/dimensionless_numbers.py
@@ -0,0 +1,52 @@
+"""Dimensionless numbers computation."""
+
+import numpy as np
+
+
+def compute_rossby(
+    velocity_scale: float,
+    f0: float,
+    length_scale: float,
+) -> float:
+    """Compute Rossby Number (Ro).
+
+    Args:
+        velocity_scale (float): Velocity scale, unit: m.s⁻¹.
+        f0 (float): Coriolis parameter, unit: s⁻¹.
+        length_scale (float): Length scale, unit: m.
+
+    Returns:
+        float: Rossby number: U/(f * L).
+    """
+    return velocity_scale / (f0 * length_scale)
+
+
+def compute_burger(
+    g: float, h_scale: float, f0: float, length_scale: float
+) -> float:
+    """Compute Burger Number (Bu).
+
+    Args:
+        g (float): Gravity acceleration, unit: m.s⁻².
+        h_scale (float): Vertical length scale, unit: m.
+        f0 (float): Coriolis parameter, unit: s⁻¹.
+        length_scale (float): Length scale, unit: m.
+
+    Returns:
+        float: Burger number: (g * H) / (f0² * Ld²)
+    """
+    return (g * h_scale) / (f0 * length_scale)
+
+
+def compute_deformation_radius(g: float, h_scale: float, f0: float) -> float:
+    """Compute Deformation Radius (Rd).
+
+    Args:
+        g (float): Gravity acceleration, unit: m.s⁻².
+        h_scale (float): Vertical length scale, unit: m.
+        f0 (float): Coriolis parameter, unit: s⁻¹.
+
+    Returns:
+        float: Deformation Radius: √(g * H) / f0
+    """
+    return np.sqrt(g * h_scale) / f0
