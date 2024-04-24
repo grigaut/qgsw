870cdcbeeb2c4b105c135c8764bfb7b34b5c0143
Gaétan Rigaut <gaetanrigaut.s@gmail.com>
2024-04-24 10:02:57 +0200

    Access display from verbose module

--- src/qgsw/verbose/__init__.py
+++ src/qgsw/verbose/__init__.py
@@ -12,9 +12,10 @@ And one can set verbose levl with:
 >>> verbose.set_level(2)
 """
 
-from qgsw.verbose._core import get_level, set_level, set_prefix
+from qgsw.verbose._core import display, get_level, set_level, set_prefix
 
 __all__ = [
+    "display",
     "get_level",
     "set_level",
     "set_prefix",
