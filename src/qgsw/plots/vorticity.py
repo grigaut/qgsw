f2e98f1d03004591afa070b0971bf12297f91f13
Gaétan Rigaut <gaetanrigaut.s@gmail.com>
2024-04-25 15:12:56 +0200

    Use AxesManagers in vorticity plots

--- src/qgsw/plots/vorticity.py
+++ src/qgsw/plots/vorticity.py
@@ -5,9 +5,17 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import numpy as np
-from typing_extensions import ParamSpec
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+from typing_extensions import ParamSpec, Self
 
-from qgsw.plots.base import BaseAxes, BaseSinglePlot
+from qgsw.models.sw import SW
+from qgsw.plots.base import (
+    BaseAxes,
+    BaseAxesContent,
+    BaseAxesContext,
+    BaseSinglePlot,
+)
 
 if TYPE_CHECKING:
     from matplotlib.axes import Axes
@@ -18,31 +26,21 @@ if TYPE_CHECKING:
 P = ParamSpec("P")
 
 
-class SurfaceVorticityAxes(BaseAxes):
-    """Surface Vorticity Axes."""
+class SurfaceVortictyAxesContext(BaseAxesContext):
+    """Axes Context Manager for surface vorticity plot."""
 
-    def _format_data(self, data: np.ndarray) -> np.ndarray:
-        """Format vorticity input.
+    _default_title = r"$\omega$"
 
-        Args:
-            data (np.ndarray): (1,nl,nx,ny) Vorticity grid.
-
-        Returns:
-            np.ndarray: Surface Vorticity layer (nx,ny).
-        """
-        return data[0, 0]
-
-    @staticmethod
-    def create_axes(figure: Figure) -> Axes:
-        """Create axes on the given figure.
+    def _create_axes(self, figure: Figure) -> Axes:
+        """Create Axes on a given figure.
 
         Args:
-            figure (Figure): Figure to add axes to.
+            figure (Figure): Figure to add Axes to.
 
         Returns:
             Axes: Added Axes.
         """
-        return figure.add_subplot()
+        return super()._create_axes(figure)
 
     def _style_axes(self, ax: Axes) -> Axes:
         """Style Axes.
@@ -51,67 +49,120 @@ class SurfaceVorticityAxes(BaseAxes):
             ax (Axes): Axes to style.
 
         Returns:
-            Axes: Styled axes.
+            Axes: Styled Axes.
         """
-        ax.set_title(r"$\omega$")
         ax.set_xticks([])
         ax.set_yticks([])
-        return ax
+        return super()._style_axes(ax)
 
-    def _set_content(self, ax: Axes) -> Axes:
-        """Set Axes content.
+
+class SurfaceVorticityAxesContent(BaseAxesContent):
+    """Axes Content Manager for surface vorticity plot."""
+
+    def _format_data(self, data: np.ndarray) -> np.ndarray:
+        """Format input data.
 
         Args:
-            ax (Axes): Axes to set the content of.
+            data (np.ndarray): Input data (1,nl,nx,ny).
 
         Returns:
-            Axes: Axes with the content set.
+            np.ndarray: Formatted data (nx,ny).
         """
-        masked_data = np.ma.masked_where(self._mask, self._data).T
+        return data[0, 0]
+
+    def _update(self, ax: Axes, data: np.ndarray, mask: np.ndarray) -> Axes:
+        """Update the Axes content.
+
+        Args:
+            ax (Axes): Axes to update.
+            data (np.ndarray): Data to use for update (nx,ny).
+            mask (np.ndarray): Mask to apply on data.
+
+        Returns:
+            Axes: Updated Axes.
+        """
+        masked_data = np.ma.masked_where(mask, data).T
         ax.imshow(masked_data, **self._kwargs)
         return ax
 
-
-class SurfaceVorticityPlot(BaseSinglePlot):
-    """Surface Vorticity Plot."""
-
-    def _create_axes(self, figure: Figure) -> Axes:
-        """Create Axes on the given Figure.
-
-        Args:
-            figure (Figure): Figure to add Axes to.
-
-        Returns:
-            Axes: Created Axes.
-        """
-        return SurfaceVorticityAxes.create_axes(figure=figure)
-
     def _retrieve_data_from_model(self, model: SW) -> np.ndarray:
-        """Retrieve vorticity data from a SW model.
+        """Retrieve data from a SW model.
 
         Args:
-            model (SW): Model to retrieve vorticity from.
+            model (SW): SW model.
 
         Returns:
-            np.ndarray: Vorticity fiedl (1,nl,nx,ny)-shaped.
+            np.ndarray: Retrieved data (1,nl,nx,ny).
         """
         omega = model.omega
         area = model.area
         f0 = model.f0
         return (omega / area / f0).cpu().numpy()
 
-    def _update(
-        self, data: np.ndarray, mask: np.ndarray, **kwargs: P.kwargs
-    ) -> Axes:
-        """Update the content of the vorticity plot.
+    def update(self, ax: Axes, data: np.ndarray, **kwargs: P.kwargs) -> Axes:
+        """Update Axes content.
 
         Args:
-            data (np.ndarray): Vorticity data (1,nl,nx,ny)-shaped.
-            mask (np.ndarray): Mask to apply on data.
-            **kwargs: additional argument to pass to ax.imshow
+            ax (Axes): Axes to update.
+            data (np.ndarray): Data to use (1,nl,nx,ny).
+            **kwargs: Additional arguments to give to the plotting function.
 
         Returns:
             Axes: Updated Axes.
         """
-        content = SurfaceVorticityAxes(data, mask, **kwargs)
-        return content.add_to_axes(ax=self.ax)
+        return super().update(ax, data, **kwargs)
+
+
+class SurfaceVorticityAxes(
+    BaseAxes[SurfaceVortictyAxesContext, SurfaceVorticityAxesContent]
+):
+    """Surface Vorticity Axes."""
+
+    def update(self, data: np.ndarray, **kwargs: P.kwargs) -> Axes:
+        """Update Axes content.
+
+        Args:
+            data (np.ndarray): Data to use (1,nl,nx,ny).
+            **kwargs: Additional arguments to give to the plotting function.
+
+        Returns:
+            Axes: Updated Axes.
+        """
+        return super().update(data, **kwargs)
+
+
+class SurfaceVorticityPlot(BaseSinglePlot[SurfaceVorticityAxes]):
+    """Surface Vorticity Plot."""
+
+    def _create_figure(self) -> Figure:
+        return super()._create_figure()
+
+    def update_with(self, data: np.ndarray, **kwargs: P.kwargs) -> None:
+        """Update Axes content.
+
+        Args:
+            data (np.ndarray): Data to use (1,nl,nx,ny).
+            **kwargs: Additional arguments to give to the plotting function.
+        """
+        return super().update_with(data, **kwargs)
+
+    @classmethod
+    def from_mask(
+        cls, mask: np.ndarray | None = None, **kwargs: P.kwargs
+    ) -> Self:
+        """Instantiate Plot only from the mask.
+
+        Args:
+            mask (np.ndarray | None, optional): Mask to apply on data.
+            Mask will be set to ones if None. Defaults to None.
+            **kwargs: Additional arguments to pass to plotting method.
+
+        Returns:
+            Self: Instantiated plot.
+        """
+        return cls(
+            axes_manager=SurfaceVorticityAxes(
+                context=SurfaceVortictyAxesContext(),
+                content=SurfaceVorticityAxesContent(mask=mask, **kwargs),
+            )
+        )
