f37b69dadf2eba3cd81aef3f86fc4eae9c1135ba
grigaut <70752330+grigaut@users.noreply.github.com>
2024-05-02 11:46:23 +0200

    Load storage path from .env (#46)

---
+++ src/qgsw/utils/storage.py
@@ -0,0 +1,35 @@
+"""Storage parsing."""
+
+import os
+from pathlib import Path
+
+
+class StorageError(Exception):
+    """Storage-related exception."""
+
+
+def get_storage_path(key: str = "STORAGE") -> Path:
+    """Read .env to find storage path.
+
+    Returns:
+        Path: $STORAGE from .env or $PWD if no STORAGE environment variable.
+    """
+    if key in os.environ:
+        return Path(os.environ[key])
+    msg = f"Impossible to read the {key} from environment variables."
+    raise StorageError(msg)
+
+
+def get_absolute_storage_path(path: Path) -> Path:
+    """Make an absolute stroage path.
+
+    Args:
+        path (Path): Path to use to save data.
+
+    Returns:
+        Path: Absolute storage path.
+    """
+    if path.is_absolute():
+        msg = f"Path {path} is absolute, use relative path instead."
+        raise StorageError(msg)
+    return get_storage_path().joinpath(path)
