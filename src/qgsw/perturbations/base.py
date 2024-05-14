b787e9f9d693027256fb2c3cad02631cb6ef6fde
Gaétan Rigaut <gaetanrigaut.s@gmail.com>
2024-05-14 11:18:22 +0200

    Regroup vortex in perturbations

--- src/qgsw/perturbations/base.py
+++ src/qgsw/perturbations/base.py
@@ -4,17 +4,109 @@ from abc import ABCMeta, abstractmethod
 
 import torch
 
+from qgsw.mesh.mesh import Mesh3D
+
+
+def grad_perp(f: torch.Tensor, dx: float, dy: float) -> torch.Tensor:
+    """Orthogonal gradient."""
+    return (f[..., :-1] - f[..., 1:]) / dy, (
+        f[..., 1:, :] - f[..., :-1, :]
+    ) / dx
+
 
 class _Perturbation(metaclass=ABCMeta):
     """Perturbation base class."""
 
     _type: str
 
+    def __init__(self, magnitude: float = 1e-3) -> None:
+        self._magnitude = magnitude
+
     @property
     def type(self) -> str:
         """Perturbation type."""
         return self._type
 
+    @property
+    def magnitude(self) -> float:
+        """Perturbation Magnitude."""
+        return self._magnitude
+
     @abstractmethod
-    def compute_initial_pressure(self) -> torch.Tensor:
-        """Compute Initial Pressure Values."""
+    def compute_scale(self, mesh: Mesh3D) -> float:
+        """Compute the scale of the perturbation.
+
+        The scale refers to the typical size of the perturbation, not its
+        magnitude. For example the scale of a vortex pertubation would be its
+        radius.
+
+        Args:
+            mesh (Mesh3D): 3D Mesh.
+
+        Returns:
+            float: Perturbation scale.
+        """
+
+    @abstractmethod
+    def compute_stream_function(self, mesh: Mesh3D) -> torch.Tensor:
+        """Compute the stream function induced by the perturbation.
+
+        Args:
+            mesh (Mesh3D): 3D Mesh.
+
+        Returns:
+            torch.Tensor: Stream function values.
+        """
+
+    def _convert_to_pressure(
+        self,
+        psi: torch.Tensor,
+        mesh: Mesh3D,
+        f0: float,
+        Ro: float,  # noqa: N803
+    ) -> torch.Tensor:
+        """Convert stream function to pressure.
+
+        Args:
+            psi (torch.Tensor): Stream function.
+            mesh (Mesh3D): 3D Mesh.
+            f0 (float): Coriolis Parameter.
+            Ro (float): Rossby Number.
+
+        Returns:
+            torch.Tensor: Pressure
+        """
+        u, v = grad_perp(
+            psi,
+            mesh.dx,
+            mesh.dy,
+        )
+        u_norm_max = max(torch.abs(u).max(), torch.abs(v).max())
+        # set psi amplitude to have a correct Rossby number
+        psi_norm = psi * (Ro * f0 * self.compute_scale(mesh) / u_norm_max)
+        # Return pressure value
+        return psi_norm * f0
+
+    def compute_initial_pressure(
+        self,
+        mesh: Mesh3D,
+        f0: float,
+        Ro: float,  # noqa: N803
+    ) -> torch.Tensor:
+        """Compute the initial pressure values.
+
+        Args:
+            mesh (Mesh3D): 3D Mesh.
+            f0 (float): Coriolis parameter.
+            Ro (float): Rossby Number.
+
+        Returns:
+            torch.Tensor: Pressure values.
+        """
+        psi = self._compute_stream_function(mesh=mesh)
+        return self._convert_to_pressure(
+            psi=psi,
+            mesh=mesh,
+            f0=f0,
+            Ro=Ro,
+        )
--- src/qgsw/perturbations/vortex.py
+++ src/qgsw/perturbations/vortex.py
@@ -4,23 +4,13 @@ from abc import ABCMeta, abstractmethod
 
 import torch
 import torch.nn.functional as F  # noqa: N812
-from typing_extensions import Self
 
-from qgsw.configs.mesh import MeshConfig
-from qgsw.configs.models import ModelConfig
-from qgsw.configs.perturbation import PerturbationConfig
-from qgsw.mesh.meshes import Meshes2D, Meshes3D
+from qgsw.mesh.mesh import Mesh2D, Mesh3D
 from qgsw.models.core import helmholtz
+from qgsw.perturbations.base import _Perturbation
 from qgsw.spatial.units._units import METERS, Unit
 from qgsw.spatial.units.exceptions import UnitError
 from qgsw.specs import DEVICE
-
-
-def grad_perp(f: torch.Tensor, dx: float, dy: float) -> torch.Tensor:
-    """Orthogonal gradient."""
-    return (f[..., :-1] - f[..., 1:]) / dy, (
-        f[..., 1:, :] - f[..., :-1, :]
-    ) / dx
 
 
 class RankineVortex2D:
@@ -31,7 +21,6 @@ class RankineVortex2D:
 
     def __init__(
         self,
-        mesh: Meshes2D,
         perturbation_magnitude: float = 1e-3,
     ) -> None:
         """Instantiate Vortex.
@@ -41,275 +30,178 @@ class RankineVortex2D:
             perturbation_magnitude (float, optional): Tripolar perturbation
             magnitude. Defaults to 1e-3.
         """
-        if mesh.xy_unit != self._required_xy_unit:
-            msg = f"XY units should be {self._required_xy_unit}."
-            raise UnitError(msg)
-        self._mesh = mesh
         self._perturbation = perturbation_magnitude
-        self._compute_psi()
-
-    @property
-    def mesh(self) -> Meshes2D:
-        """Underlying mesh."""
-        return self._mesh
 
     @property
     def perturbation_magnitude(self) -> float:
         """Tripolar perturbation magnitude."""
         return self._perturbation
 
-    @property
-    def r0(self) -> float:
-        """Core cylinder radius: 0.1*lx."""
-        return 0.1 * self._mesh.lx
+    def _raise_if_invalid_unit(self, mesh: Mesh2D) -> None:
+        """Check the mesh unit.
 
-    @property
-    def r1(self) -> float:
-        """Inner radius of the surrounding ring: r0."""
-        return self.r0
+        Args:
+            mesh (Mesh2D): 2D Mesh.
 
-    @property
-    def r2(self) -> float:
-        """Outer radius of the surrounding ring: 0.14*lx."""
-        return 0.14 * self._mesh.lx
-
-    @property
-    def psi(self) -> torch.Tensor:
-        """Value of the stream function ψ.
-
-        The Tensor has a shape of (1, 1, nx + 1, ny + 1).
+        Raises:
+            UnitError: If the mesh has an invalid unit
         """
-        return self._psi
+        if mesh.xy_unit != self._required_xy_unit:
+            msg = (
+                "The mesh must have the following xy unit:"
+                f" {self._required_xy_unit.name}"
+            )
+            raise UnitError(msg)
 
-    def _compute_psi(self) -> torch.Tensor:
+    def compute_vortex_scales(
+        self,
+        mesh: Mesh2D,
+    ) -> tuple[float, float, float]:
+        """Compute the vortex radiuses.
+
+        R0 is the radius of the innermost gyre,
+        R1 is the radius of the inner part of the surrounding ring,
+        R2 is the radius of the outermost part of the ring.
+
+        Args:
+            mesh (Mesh3D): 3D Mesh.
+
+        Returns:
+            tuple[float, float, float]: R0, R1, R2.
+        """
+        self._raise_if_invalid_unit(mesh)
+        r0 = 0.1 * mesh.lx
+        r1 = r0
+        r2 = 0.14 * mesh.lx
+        return r0, r1, r2
+
+    def compute_stream_function(self, mesh: Mesh2D) -> torch.Tensor:
         """Compute the value of the streamfunction ψ.
 
         Returns:
             torch.Tensor: Streamfunction values over the
         (nx + 1, ny + 1) domain.
         """
-        vor = self._compute_vorticity(self._mesh.omega.xy)
+        vor = self._compute_vorticity(mesh)
         # Compute Laplacian operator in Fourier Space
         laplacian = helmholtz.compute_laplace_dstI(
-            self._mesh.nx,
-            self._mesh.ny,
-            self._mesh.dx,
-            self._mesh.dy,
+            mesh.nx - 1,
+            mesh.ny - 1,
+            mesh.dx,
+            mesh.dy,
             {"device": DEVICE, "dtype": torch.float64},
         )
         # Solve problem in Fourier space : "ψ = ω/∆"
         psi_hat = helmholtz.dstI2D(vor[1:-1, 1:-1]) / laplacian
         # Come back to original space
         psi = F.pad(helmholtz.dstI2D(psi_hat), (1, 1, 1, 1))
-        self._psi = psi.unsqueeze(0).unsqueeze(0)
+        return psi.unsqueeze(0).unsqueeze(0)
 
     def _compute_vorticity(
         self,
-        mesh_xy: tuple[torch.Tensor, torch.Tensor],
+        mesh: Mesh2D,
     ) -> torch.Tensor:
         """Compute the vorticity ω of the vortex.
 
         Args:
-            mesh_xy (tuple[torch.Tensor, torch.Tensor]): Grid.
+            mesh (Mesh2D): Grid.
 
         Returns:
             torch.Tensor: Vorticity Value.
         """
-        x, y = mesh_xy
+        x, y = mesh.xy
+        r0, r1, r2 = self.compute_vortex_scales(mesh=mesh)
         # Compute cylindrical components
         theta = torch.angle(x + 1j * y)
         r = torch.sqrt(x**2 + y**2)
         r = r * (1 + self._perturbation * torch.cos(theta * 3))
         # Mask vortex's core
-        mask_core = torch.sigmoid((self.r0 - r) / self._norm_factor)
+        mask_core = torch.sigmoid((r0 - r) / self._norm_factor)
         # Mask vortex's ring
-        inner_ring = torch.sigmoid((r - self.r1) / self._norm_factor)
-        outer_ring = torch.sigmoid((self.r2 - r) / self._norm_factor)
+        inner_ring = torch.sigmoid((r - r1) / self._norm_factor)
+        outer_ring = torch.sigmoid((r2 - r) / self._norm_factor)
         mask_ring = inner_ring * outer_ring
         # compute vorticity
         return mask_ring / mask_ring.mean() - mask_core / mask_core.mean()
 
 
-class RankineVortex3D(metaclass=ABCMeta):
+class RankineVortex3D(_Perturbation, metaclass=ABCMeta):
     """3D Rankine Vortex."""
 
-    def __init__(
-        self,
-        mesh: Meshes3D,
-        perturbation_magnitude: float = 1e-3,
-    ) -> None:
-        """Instantiate 3D Rankine Vortex.
+    def __init__(self, magnitude: float = 0.001) -> None:
+        """Instantiate 3D Vortex.
 
         Args:
-            mesh (Meshes3D): 3D Grid.
-            perturbation_magnitude (float, optional): Tripolar perturbation
-            magnitude. Defaults to 1e-3.
+            magnitude (float, optional): Tripolar perturbation magnitude.
+              Defaults to 0.001.
         """
-        self._mesh = mesh
-        self._2d = RankineVortex2D(
-            mesh=mesh.remove_z_h(),
-            perturbation_magnitude=perturbation_magnitude,
-        )
-
-    @property
-    def mesh(self) -> Meshes3D:
-        """Underlying mesh."""
-        return self._mesh
+        super().__init__(magnitude)
+        self._2d_vortex = RankineVortex2D(perturbation_magnitude=magnitude)
 
     @property
     def perturbation_magnitude(self) -> float:
         """Tripolar perturbation magnitude."""
-        return self._2d.perturbation_magnitude
+        return self._2d_vortex.perturbation_magnitude
 
-    @property
-    def r0(self) -> float:
-        """Core cylinder radius: 0.1*lx."""
-        return self._2d.r0
+    def compute_scale(self, mesh: Mesh3D) -> float:
+        """Compute Vortex Scale.
 
-    @property
-    def r1(self) -> float:
-        """Inner radius of the surrounding ring: r0."""
-        return self._2d.r1
+        Args:
+            mesh (Mesh3D): 3D Mesh
 
-    @property
-    def r2(self) -> float:
-        """Outer radius of the surrounding ring: 0.14*lx."""
-        return self._2d.r2
+        Returns:
+            float: Inner vortex gyre radius.
+        """
+        return self._2d_vortex.compute_vortex_scales(mesh.remove_z_h())[0]
 
-    @property
     @abstractmethod
-    def psi(self) -> torch.Tensor:
+    def compute_stream_function(self, mesh: Mesh3D) -> torch.Tensor:
         """Value of the stream function ψ.
 
-        The Tensor has a shape of (1, nl, nx + 1, ny + 1).
+        Args:
+            mesh (Mesh3D): 3D Mesh to generate Stream Function on.
+
+        Returns:
+            torch.Tensor: Stream function values.
         """
-        xy_shape = self._2d.psi.shape[-2:]
-        return self._2d.psi.expand((1, self._mesh.nl, *xy_shape))
 
 
 class ActiveLayersRankineVortex3D(RankineVortex3D):
     """3D Rankine Vortex with similar vortex behavior accross all layers."""
 
-    @property
-    def psi(self) -> torch.Tensor:
+    def compute_stream_function(self, mesh: Mesh3D) -> torch.Tensor:
         """Value of the stream function ψ.
 
-        The Tensor has a shape of (1, nl, nx + 1, ny + 1).
+        Args:
+            mesh (Mesh3D): 3D Mesh to generate Stream Function on.
+
+        Returns:
+            torch.Tensor: Stream function values.
         """
-        xy_shape = self._2d.psi.shape[-2:]
-        return self._2d.psi.expand((1, self._mesh.nl, *xy_shape))
+        psi_2d = self._2d_vortex.compute_stream_function(mesh.remove_z_h())
+        nx, ny = psi_2d.shape[-2:]
+        return psi_2d.expand((1, mesh.nl, nx, ny))
 
 
 class PassiveLayersRankineVortex3D(RankineVortex3D):
     """3D Rankine Vortex with only superior layer active."""
 
-    @property
-    def psi(self) -> torch.Tensor:
+    def compute_stream_function(self, mesh: Mesh3D) -> torch.Tensor:
         """Value of the stream function ψ.
 
-        The Tensor has a shape of (1, nl, nx + 1, ny + 1).
+        Args:
+            mesh (Mesh3D): 3D Mesh to generate Stream Function on.
+
+        Returns:
+            torch.Tensor: Stream function values.
         """
-        xy_shape = self._2d.psi.shape[-2:]
+        psi_2d = self._2d_vortex.compute_stream_function(mesh.remove_z_h())
+        nx, ny = psi_2d.shape[-2:]
         psi = torch.ones(
-            (1, self._mesh.nl, *xy_shape),
+            (1, mesh.nl, nx, ny),
             device=DEVICE,
             dtype=torch.float64,
         )
-        psi[0, 0, ...] = self._2d.psi
+
+        psi[0, 0, ...] = psi_2d
         return psi
-
-
-class RankineVortexForcing:
-    """Vortex Forcing's abtract class."""
-
-    def __init__(self, vortex: RankineVortex3D) -> None:
-        """Instantiate Vortex.
-
-        Args:
-            vortex (RankineVortex3D): Corresponding Vortex
-        """
-        self._vortex = vortex
-
-    @property
-    def perturbation_magnitude(self) -> float:
-        """Tripolar perturbation magnitude."""
-        return self._vortex.perturbation_magnitude
-
-    @property
-    def r0(self) -> float:
-        """Core cylinder radius: 0.1*lx."""
-        return self._vortex.r0
-
-    @property
-    def r1(self) -> float:
-        """Inner radius of the surrounding ring: r0."""
-        return self._vortex.r1
-
-    @property
-    def r2(self) -> float:
-        """Outer radius of the surrounding ring: 0.14*lx."""
-        return self._vortex.r2
-
-    def compute(self, f0: float, Ro: float) -> torch.Tensor:  # noqa: N803
-        """Compute Initial pressure based on the vortex's vorticity.
-
-        Args:
-            f0 (float): Coriolis Parameter from β-plane approximation.
-            Ro (float): Rossby Number.
-
-        Returns:
-            torch.Tensor: Initial Pressure with the presence of the vortex.
-        """
-        u, v = grad_perp(
-            self._vortex.psi,
-            self._vortex.mesh.dx,
-            self._vortex.mesh.dy,
-        )
-        u_norm_max = max(torch.abs(u).max(), torch.abs(v).max())
-        # set psi amplitude to have a correct Rossby number
-        psi_norm = self._vortex.psi * (Ro * f0 * self._vortex.r0 / u_norm_max)
-        # Return pressure value
-        return psi_norm * f0
-
-    @classmethod
-    def from_config(
-        cls,
-        perturbation_config: PerturbationConfig,
-        mesh_config: MeshConfig,
-        model_config: ModelConfig,
-    ) -> Self:
-        """Instantiate VortexForcing from ScriptConfig.
-
-        Args:
-            perturbation_config (PerturbationConfig): Vortex configuration.
-            mesh_config (MeshConfig): Mesh configuration.
-            model_config (ModelConfig): Model configuration.
-
-        Raises:
-            KeyError: If the vortex is not recognized.
-
-        Returns:
-            Self: Vortex forcing.
-        """
-        if perturbation_config.type == "active":
-            mesh = Meshes3D.from_config(
-                mesh_config=mesh_config,
-                model_config=model_config,
-            )
-            vortex = ActiveLayersRankineVortex3D(
-                mesh=mesh,
-                perturbation_magnitude=perturbation_config.perturbation_magnitude,
-            )
-            return cls(vortex=vortex)
-        if perturbation_config.type == "passive":
-            mesh = Meshes3D.from_config(
-                mesh_config=mesh_config,
-                model_config=model_config,
-            )
-            vortex = PassiveLayersRankineVortex3D(
-                mesh=mesh,
-                perturbation_magnitude=perturbation_config.perturbation_magnitude,
-            )
-            return cls(vortex=vortex)
-        msg = "Unrecognized vortex type."
-        raise KeyError(msg)
