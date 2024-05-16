109adc0b3c65674e79e072d60f140e6b485bdd82
Gaétan Rigaut <gaetanrigaut.s@gmail.com>
2024-05-16 12:30:01 +0200

    Create dim_1, dim_2 and dim_3 modules

---
+++ src/qgsw/spatial/dim_3/__init__.py
@@ -0,0 +1,7 @@
+"""3D space-related objects."""
+
+from qgsw.spatial.core.coordinates import Coordinates3D
+from qgsw.spatial.core.mesh import Mesh3D
+from qgsw.spatial.core.meshes import SpaceDiscretization3D
+
+__all__ = ["Coordinates3D", "Mesh3D", "SpaceDiscretization3D"]
