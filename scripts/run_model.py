18e96eaaf05e1e41150e9d94f87f5ec2ac510d75
Gaétan Rigaut <gaetanrigaut.s@gmail.com>
2024-06-06 17:06:00 +0200

    Don't specify device and dtype

--- scripts/run_model.py
+++ scripts/run_model.py
@@ -75,8 +75,6 @@ params = {
     "taux": taux,
     "tauy": tauy,
     "bottom_drag_coef": config.physics.bottom_drag_coef,
-    "device": DEVICE,
-    "dtype": torch.float64,
     "mask": torch.ones_like(space.h.remove_z_h().xy.x),
     "compile": True,
     "slip_coef": config.physics.slip_coef,
