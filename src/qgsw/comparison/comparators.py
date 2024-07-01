33a45ecf213f93ac2dca9d15f3f0c679a382410a
Gaétan Rigaut <gaetanrigaut.s@gmail.com>
2024-07-01 15:22:14 +0200

    Implement comparison functions

--- src/qgsw/comparison/comparators.py
+++ src/qgsw/comparison/comparators.py
@@ -1,41 +1,30 @@
 """Comparison functions."""
 
-from typing import Callable
-
 import torch
+from torch.nn import MSELoss
 
 
-class Comparator:
-    """Comparator."""
+def rmse(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    """RMSE.
 
-    def __init__(self) -> None:
-        """Instantiate Comparator."""
+    Args:
+        x (torch.Tensor): First tensor to compare.
+        y (torch.Tensor): Second tensor to compare.
 
-    def _raise_if_unmatching_size(
-        self,
-        top: torch.Tensor,
-        bottom: torch.Tensor,
-    ) -> None:
-        if top.shape != bottom.shape:
-            msg = "Top and Bottom tensor shapes don't match."
-            raise ValueError(msg)
+    Returns:
+        torch.Tensor: Scalar: sqrt(mean((x-y)**2)).
+    """
+    return torch.sqrt(MSELoss()(x, y))
 
-    def _compute_mask(
-        self,
-        top: torch.Tensor,
-        bottom: torch.Tensor,
-    ) -> torch.Tensor: ...
 
-    def compare(self, top: torch.Tensor, bottom: torch.Tensor) -> Callable:
-        """Compare.
+def absolute_difference(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    """Absolute difference.
 
-        Args:
-            top (torch.Tensor): _description_
-            bottom (torch.Tensor): _description_
+    Args:
+        x (torch.Tensor): First tensor to compare.
+        y (torch.Tensor): Second tensor to compare.
 
-        Returns:
-            Callable: _description_
-        """
-        self._raise_if_unmatching_size(top, bottom)
-        mask = self._compute_mask(top, bottom)
-        return self._compare(top.where(mask), bottom.where(mask))
+    Returns:
+        torch.Tensor: The output has the size of the inputs and is |x-y|.
+    """
+    return torch.abs(x - y)
