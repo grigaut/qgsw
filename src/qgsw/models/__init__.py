168200c44fe5aff21d62a98994d7e493d6e14bc4
grigaut <gaetanrigaut.s@gmail.com>
2024-12-19 10:42:07 +0100

    Remove models import from models/__init__.py

--- src/qgsw/models/__init__.py
+++ src/qgsw/models/__init__.py
@@ -1,17 +1,1 @@
 """Models."""
-
-from qgsw.models.qg.collinear_sublayer import QGCollinearSF
-from qgsw.models.qg.core import QG
-from qgsw.models.sw.core import SW
-from qgsw.models.sw.filtering import (
-    SWFilterBarotropicExact,
-    SWFilterBarotropicSpectral,
-)
-
-__all__ = [
-    "QG",
-    "QGCollinearSF",
-    "SW",
-    "SWFilterBarotropicSpectral",
-    "SWFilterBarotropicExact",
-]
