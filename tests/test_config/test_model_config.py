215bb5b8af2d94fc7964afc587fa50a16a4ffa7c
grigaut <gaetanrigaut.s@gmail.com>
2024-12-21 12:10:57 +0100

    Refactor tests

--- tests/test_config/test_model_config.py
+++ tests/test_config/test_model_config.py
@@ -1,25 +1,39 @@
 """Test model configurations."""
 
+import pytest
 import torch
 
 from qgsw.configs.models import ModelConfig
 from qgsw.specs import DEVICE
 
+DEVICE.set_manually("cpu")
 
-def test_h_g_prime() -> None:
-    """Test H and g' shapes, dtype and device."""
-    DEVICE.set_manually("cpu")
 
-    config = ModelConfig(
+@pytest.fixture
+def model_config() -> ModelConfig:
+    """Define model config."""
+    return ModelConfig(
         type="QG",
         prefix="",
         layers=[100, 200],
         reduced_gravity=[10, 0.05],
         collinearity_coef=None,
     )
-    assert config.h.shape == (2,)
-    assert config.g_prime.shape == (2,)
-    assert config.h.dtype == torch.float64
-    assert config.g_prime.dtype == torch.float64
-    assert config.h.device == DEVICE.get()
-    assert config.g_prime.device == DEVICE.get()
+
+
+def test_h_g_prime_shape(model_config: ModelConfig) -> None:
+    """Test H and g' dtype and device."""
+    assert model_config.h.shape == (2,)
+    assert model_config.g_prime.shape == (2,)
+
+
+def test_h_g_prime_dtype(model_config: ModelConfig) -> None:
+    """Test H and g' dtype."""
+    assert model_config.h.dtype == torch.float64
+    assert model_config.g_prime.dtype == torch.float64
+
+
+def test_h_g_prime_device(model_config: ModelConfig) -> None:
+    """Test H and g' device."""
+    assert model_config.h.device == DEVICE.get()
+    assert model_config.g_prime.device == DEVICE.get()
