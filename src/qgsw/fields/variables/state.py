f72b9cc0f97fbd5f4644e0acca539abca13f1088
grigaut <gaetanrigaut.s@gmail.com>
2025-01-10 11:24:56 +0100

    Create Statealpha

--- src/qgsw/fields/variables/state.py
+++ src/qgsw/fields/variables/state.py
@@ -2,7 +2,8 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Generic, TypeVar
 
 try:
     from typing import Self
@@ -11,11 +12,12 @@ except ImportError:
 
 
 from qgsw.fields.variables.prognostic import (
+    CollinearityCoefficient,
     LayerDepthAnomaly,
     MeridionalVelocity,
     ZonalVelocity,
 )
-from qgsw.fields.variables.uvh import UVH
+from qgsw.fields.variables.uvh import UVH, UVHalpha
 
 if TYPE_CHECKING:
     import torch
@@ -26,22 +28,17 @@ if TYPE_CHECKING:
     )
 
 
-class State:
-    """State: wrapper for UVH state variables.
+T = TypeVar("T", bound=UVH | UVHalpha)
 
-    This wrapper links uvh variables to diagnostic variables.
-    Diagnostic variables can be bound to the state so that they are updated
-    only when the state has changed.
-    """
 
-    def __init__(
-        self,
-        uvh: UVH,
-    ) -> None:
-        """Instantiate state.
+class BaseState(ABC, Generic[T]):
+    """Base State class."""
+
+    def __init__(self, uvh: T) -> None:
+        """Instantiate State.
 
         Args:
-            uvh (UVH): Prognostic variables.
+            uvh (T): Core UVH or UVHalpha.
         """
         self.unbind()
         self._uvh = uvh
@@ -53,19 +50,6 @@ class State:
             MeridionalVelocity.get_name(): self._v,
             LayerDepthAnomaly.get_name(): self._h,
         }
-
-    @property
-    def uvh(self) -> UVH:
-        """Prognostic variables."""
-        return self._uvh
-
-    @uvh.setter
-    def uvh(self, uvh: UVH) -> None:
-        self._uvh = uvh
-        self._u.update(uvh.u)
-        self._v.update(uvh.v)
-        self._h.update(uvh.h)
-        self._updated()
 
     @property
     def u(self) -> ZonalVelocity:
@@ -81,6 +65,17 @@ class State:
     def h(self) -> LayerDepthAnomaly:
         """Prognostic layer thickness anomaly."""
         return self._h
+
+    @property
+    def uvh(self) -> T:
+        """Prognostic variables."""
+        return self._uvh
+
+    @uvh.setter
+    def uvh(self, uvh: T) -> None:
+        self._updated()
+        self._uvh = uvh
+        self._update_prognostic_vars(uvh)
 
     @property
     def vars(self) -> dict[str, PrognosticVariable | BoundDiagnosticVariable]:
@@ -146,21 +141,6 @@ class State:
             raise KeyError(msg)
         return self.vars[name]
 
-    def update(
-        self,
-        u: torch.Tensor,
-        v: torch.Tensor,
-        h: torch.Tensor,
-    ) -> None:
-        """Update prognostic variables.
-
-        Args:
-            u (torch.Tensor): Zonal velocity.
-            v (torch.Tensor): Meriodional velocity.
-            h (torch.Tensor): Surface height anomaly.
-        """
-        self.uvh = UVH(u, v, h)
-
     def _updated(self) -> None:
         """Update diagnostic variables."""
         for var in self.diag_vars.values():
@@ -182,6 +162,23 @@ class State:
     def unbind(self) -> None:
         """Unbind all variables from state."""
         self._diag: dict[str, BoundDiagnosticVariable] = {}
+
+    @abstractmethod
+    def _update_prognostic_vars(self, uvh: T) -> None: ...
+
+
+class State(BaseState[UVH]):
+    """State: wrapper for UVH state variables.
+
+    This wrapper links uvh variables to diagnostic variables.
+    Diagnostic variables can be bound to the state so that they are updated
+    only when the state has changed.
+    """
+
+    def _update_prognostic_vars(self, uvh: UVH) -> None:
+        self._u.update(uvh.u)
+        self._v.update(uvh.v)
+        self._h.update(uvh.h)
 
     @classmethod
     def steady(
@@ -226,3 +223,81 @@ class State:
             Self: State.
         """
         return cls(UVH(u, v, h))
+
+
+class Statealpha(BaseState[UVHalpha]):
+    """Statealpha: wrapper for UVHalpha state variables.
+
+    This wrapper links uvh variables to diagnostic variables.
+    Diagnostic variables can be bound to the state so that they are updated
+    only when the state has changed.
+    """
+
+    def __init__(self, uvh: UVHalpha) -> None:
+        """Instantiate Statealpha.
+
+        Args:
+            uvh (UVHalpha): Core prognostic variables.
+        """
+        super().__init__(uvh)
+        self._alpha = CollinearityCoefficient(uvh.alpha)
+        self._prog[CollinearityCoefficient.get_name()] = self._alpha
+
+    @property
+    def alpha(self) -> CollinearityCoefficient:
+        """Collinearity coefficient."""
+        return self._alpha
+
+    def _update_prognostic_vars(self, uvh: UVHalpha) -> None:
+        self._u.update(uvh.u)
+        self._v.update(uvh.v)
+        self._h.update(uvh.h)
+        self._alpha.update(uvh.alpha)
+
+    @classmethod
+    def steady(
+        cls,
+        alpha: torch.Tensor,
+        n_ens: int,
+        nl: int,
+        nx: int,
+        ny: int,
+        dtype: torch.dtype,
+        device: torch.device,
+    ) -> Self:
+        """Instantiate a steady state with zero-filled prognostic variables.
+
+        Args:
+            alpha (torch.Tensor): Collinearity coefficient.
+            n_ens (int): Number of ensembles.
+            nl (int): Number of layers.
+            nx (int): Number of points in the x direction.
+            ny (int): Number of points in the y direction.
+            dtype (torch.dtype): Data type.
+            device (torch.device): Device to use.
+
+        Returns:
+            Self: State.
+        """
+        return cls(UVHalpha.steady(alpha, n_ens, nl, nx, ny, dtype, device))
+
+    @classmethod
+    def from_tensors(
+        cls,
+        u: torch.Tensor,
+        v: torch.Tensor,
+        h: torch.Tensor,
+        alpha: torch.Tensor,
+    ) -> Self:
+        """Instantiate the state from tensors.
+
+        Args:
+            u (torch.Tensor): Zonal velocity.
+            v (torch.Tensor): Meridional velocity.
+            h (torch.Tensor): Surface height anomaly.
+            alpha (torch.Tensor): Collinearity coefficient.
+
+        Returns:
+            Self: State.
+        """
+        return cls(UVHalpha(u, v, h, alpha))
