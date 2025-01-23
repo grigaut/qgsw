d3d1443d18498810554cd1a369f648f9f7dbf974
grigaut <gaetanrigaut.s@gmail.com>
2025-01-23 15:46:26 +0100

    Create test for filters

--- tests/test_filters/test_gaussian.py
+++ tests/test_filters/test_gaussian.py
@@ -5,11 +5,14 @@ import pytest
 import torch
 from scipy import signal
 
+from qgsw.filters.base import _Filter
 from qgsw.filters.gaussian import (
     GaussianFilter,
     GaussianFilter1D,
     GaussianFilter2D,
 )
+from qgsw.filters.high_pass import GaussianHighPass1D, GaussianHighPass2D
+from qgsw.filters.low_pass import GaussianLowPass1D, GaussianLowPass2D
 from qgsw.specs import DEVICE
 
 testdata = [
@@ -34,3 +37,33 @@ def test_gaussian_filter(
     )
     assert y_filt.shape == y.shape
     assert np.isclose(y_filt, y_filt_ref).all()
+
+
+testdata = [
+    pytest.param(
+        (500,),
+        GaussianLowPass1D(10),
+        GaussianHighPass1D(10),
+        id="1D",
+    ),
+    pytest.param(
+        (500, 500),
+        GaussianLowPass2D(10),
+        GaussianHighPass2D(10),
+        id="2D",
+    ),
+]
+
+
+@pytest.mark.parametrize(("input_shape", "filt_lp", "filt_hp"), testdata)
+def test_lp_hp(
+    input_shape: tuple[int, ...],
+    filt_lp: _Filter,
+    filt_hp: _Filter,
+) -> None:
+    """Test low-pass and high-pass reconstruction."""
+    y = torch.rand(input_shape, dtype=torch.float64, device=DEVICE.get())
+
+    y_filt_lp = filt_lp(y)
+    y_filt_hp = filt_hp(y)
+    assert torch.isclose(y_filt_hp + y_filt_lp, y).all()
