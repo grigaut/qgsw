0772649e960b10ff6638516bc8dd8c5e83a2152e
grigaut <gaetanrigaut.s@gmail.com>
2025-01-23 14:33:16 +0100

    Creat Gaussian filters

--- src/qgsw/utils/gaussian_filtering.py
+++ src/qgsw/utils/gaussian_filtering.py
@@ -2,6 +2,11 @@
 
 from __future__ import annotations
 
+try:
+    from typing import Self
+except ImportError:
+    from typing_extensions import Self
+
 from abc import ABC, abstractmethod
 
 import torch
@@ -12,8 +17,8 @@ from qgsw.specs import DEVICE
 class GaussianFilter(ABC):
     """Gaussian Filter."""
 
-    _windows_width = 4  # nbs of sigma
-    _null_threshold = 0.05
+    _windows_width_factor = 4  # nbs of sigma
+    _span_threshold = 0.05
     dtype = torch.float64
 
     def __init__(self, sigma: float) -> None:
@@ -29,6 +34,16 @@ class GaussianFilter(ABC):
     def kernel(self) -> torch.Tensor:
         """Gaussian kernel."""
         return self._kernel
+
+    @property
+    def span(self) -> float:
+        """Gaussian span."""
+        return self.compute_span_from_sigma(self._sigma, self.span_threshold)
+
+    @property
+    def span_threshold(self) -> float:
+        """Span bounding threshold."""
+        return self._span_threshold
 
     @property
     def sigma(self) -> float:
@@ -47,7 +62,7 @@ class GaussianFilter(ABC):
     @property
     def window_radius(self) -> int:
         """Window radius."""
-        return int(self._windows_width * self._sigma) + 1
+        return int(self._windows_width_factor * self._sigma) + 1
 
     @property
     def window_width(self) -> int:
@@ -61,6 +76,42 @@ class GaussianFilter(ABC):
         Returns:
             torch.Tensor: mu
         """
+
+    @staticmethod
+    def compute_sigma_from_span(span: float, threshold: float) -> float:
+        """Compute the standard deviation givent the span.
+
+        Args:
+            span (float): Desired span (in px).
+            threshold (float): Threshold to bound the span.
+
+        Returns:
+            float: Corresponding standard deviation.
+        """
+        thres = torch.tensor(
+            threshold,
+            dtype=torch.float64,
+            device=DEVICE.get(),
+        )
+        return (span / torch.sqrt(-8 * torch.log(thres))).item()
+
+    @staticmethod
+    def compute_span_from_sigma(sigma: float, threshold: float) -> float:
+        """Compute the standard deviation givent the span.
+
+        Args:
+            sigma (float): Standard deviation.
+            threshold (float): Threshold to bound the span.
+
+        Returns:
+            float: Corresponding standard deviation.
+        """
+        thres = torch.tensor(
+            threshold,
+            dtype=torch.float64,
+            device=DEVICE.get(),
+        )
+        return (sigma * torch.sqrt(-8 * torch.log(thres))).item()
 
     @staticmethod
     def compute_window_width(window_radius: int) -> int:
@@ -92,6 +143,23 @@ class GaussianFilter(ABC):
             torch.Tensor: Gaussian kernel.
         """
 
+    @classmethod
+    def from_span(cls, span: float) -> Self:
+        """Instantiate the filter given the span of the gaussian.
+
+        Args:
+            span (float): Span (in px).
+
+        Returns:
+            Self: Gaussian filter.
+        """
+        return cls(
+            sigma=cls.compute_sigma_from_span(
+                span,
+                threshold=cls._span_threshold,
+            ),
+        )
+
 
 class GaussianFilter1D(GaussianFilter):
     """1D Gaussian Filter."""
@@ -113,11 +181,11 @@ class GaussianFilter1D(GaussianFilter):
 
         Args:
             sigma (float): Standard deviation.
-            mu (torch.Tensor): Expected value.
+            mu (torch.Tensor): Expected value, (1,)-shaped.
             window_radius (int): Window radius.
 
         Returns:
-            torch.Tensor: Gaussian kernel.
+            torch.Tensor: Gaussian kernel (2*windows_radius+1,)-shaped.
         """
         x = torch.arange(
             -window_radius,
@@ -136,7 +204,7 @@ class GaussianFilter2D(GaussianFilter):
 
     def _set_null_mu(self) -> torch.Tensor:
         return torch.zeros(
-            (2, 1, 1),
+            (2,),
             dtype=torch.float64,
             device=DEVICE.get(),
         )
@@ -151,11 +219,12 @@ class GaussianFilter2D(GaussianFilter):
 
         Args:
             sigma (float): Standard deviation.
-            mu (torch.Tensor): Expected value.
+            mu (torch.Tensor): Expected value, (2,)-shaped.
             window_radius (int): Window radius.
 
         Returns:
-            torch.Tensor: Gaussian kernel.
+            torch.Tensor: Gaussian kernel
+            (2*windows_radius+1,2*windows_radius+1)-shaped.
         """
         x = torch.arange(
             -window_radius,
@@ -163,7 +232,10 @@ class GaussianFilter2D(GaussianFilter):
             device=DEVICE.get(),
             dtype=torch.float64,
         )
-        r = torch.norm(torch.stack(torch.meshgrid(x, x)) - mu, dim=0)
+        xy = torch.stack(torch.meshgrid(x, x))
+        mu_reshaped = mu.reshape((-1, 1, 1))
+
+        r = torch.norm(xy - mu_reshaped, dim=0)
 
         kernel = torch.exp(-(r**2 / (2 * sigma**2)))
         kernel /= torch.sum(kernel)
