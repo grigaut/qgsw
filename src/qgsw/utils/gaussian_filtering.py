1be3499f30866d3c90f59ad9bd4cd6d998d68326
grigaut <gaetanrigaut.s@gmail.com>
2025-01-23 12:03:41 +0100

    Define 1D and 2D  Gaussian filters

--- src/qgsw/utils/gaussian_filtering.py
+++ src/qgsw/utils/gaussian_filtering.py
@@ -2,171 +2,169 @@
 
 from __future__ import annotations
 
-from abc import ABCMeta, abstractmethod
-from typing import overload
+from abc import ABC, abstractmethod
 
-import numpy as np
 import torch
-from scipy import signal
 
 from qgsw.specs import DEVICE
 
 
-class GaussianFilterBase(metaclass=ABCMeta):
-    """Base Class for Gaussian Filters."""
+class GaussianFilter(ABC):
+    """Gaussian Filter."""
 
-    def __init__(self, sigma: float, radius: int) -> None:
-        """Instantiates the Gaussian filter.
+    _windows_width = 4  # nbs of sigma
+    _null_threshold = 0.05
+    dtype = torch.float64
+
+    def __init__(self, sigma: float) -> None:
+        """Instantiate the filter.
 
         Args:
             sigma (float): Standard deviation.
-            radius (int): Kernel Radius.
         """
+        self._mu = self._set_null_mu()
         self.sigma = sigma
-        self.radius = radius
+
+    @property
+    def kernel(self) -> torch.Tensor:
+        """Gaussian kernel."""
+        return self._kernel
 
     @property
     def sigma(self) -> float:
-        """Sigma."""
+        """Standard deviation."""
         return self._sigma
 
     @sigma.setter
     def sigma(self, value: float) -> None:
-        if value <= 0:
-            msg = "Sigma must be greater than 0."
-            raise ValueError(msg)
         self._sigma = value
+        self._kernel = self.compute_kernel(
+            self._sigma,
+            mu=self._mu,
+            window_radius=self.window_radius,
+        )
 
     @property
-    def radius(self) -> int:
-        """Kernel Width."""
-        return self._radius
+    def window_radius(self) -> int:
+        """Window radius."""
+        return int(self._windows_width * self._sigma) + 1
 
-    @radius.setter
-    def radius(self, value: int) -> None:
-        if value <= 0:
-            msg = "Kernel width must be greater than 0."
-            raise ValueError(msg)
-        if not isinstance(value, int):
-            msg = "Kernel width must be an integer."
-            raise TypeError(msg)
-        self._radius = value
-        self._kernel = self._generate_kernel()
+    @property
+    def window_width(self) -> int:
+        """Window radius."""
+        return self.compute_window_width(self.window_radius)
 
     @abstractmethod
-    def _generate_kernel(self) -> np.ndarray: ...
-
-    @abstractmethod
-    def _smooth(self, y: np.ndarray) -> np.ndarray: ...
-
-    @overload
-    def smooth(self, y: np.ndarray) -> np.ndarray: ...
-
-    @overload
-    def smooth(self, y: torch.Tensor) -> torch.Tensor: ...
-
-    def smooth(
-        self,
-        y: torch.Tensor | np.ndarray,
-    ) -> torch.Tensor | np.ndarray:
-        """Smooth the given array.
-
-        Args:
-            y (torch.Tensor | np.ndarray): Array to smooth.
+    def _set_null_mu(self) -> torch.Tensor:
+        """Compute null mu.
 
         Returns:
-            torch.Tensor | np.ndarray: Smoothed array.
+            torch.Tensor: mu
         """
-        if isinstance(y, np.ndarray):
-            return self._smooth(y)
-        return torch.tensor(
-            self._smooth(y.cpu().numpy()),
+
+    @staticmethod
+    def compute_window_width(window_radius: int) -> int:
+        """Compute the wisndow width.
+
+        Args:
+            window_radius (int): Window radius.
+
+        Returns:
+            int: Window width
+        """
+        return window_radius * 2 + 1
+
+    @staticmethod
+    @abstractmethod
+    def compute_kernel(
+        sigma: float,
+        mu: torch.Tensor,
+        window_radius: int,
+    ) -> torch.Tensor:
+        """Compute the gaussian kernel.
+
+        Args:
+            sigma (float): Standard deviation.
+            mu (torch.Tensor): Expected value.
+            window_radius (int): Window radius.
+
+        Returns:
+            torch.Tensor: Gaussian kernel.
+        """
+
+
+class GaussianFilter1D(GaussianFilter):
+    """1D Gaussian Filter."""
+
+    def _set_null_mu(self) -> torch.Tensor:
+        return torch.zeros(
+            (1,),
             dtype=torch.float64,
             device=DEVICE.get(),
         )
 
-
-class GaussianFilter1D(GaussianFilterBase):
-    """1D Gaussian Filtering."""
-
-    def _generate_kernel(self) -> np.ndarray:
-        """Generate gaussian kernel.
-
-        Returns:
-            np.ndarray: gaussian kernel.
-        """
-        x = np.linspace(-1, 1, int(self.radius))
-        kernel = (
-            1
-            / (self.sigma * np.sqrt(2 * torch.pi))
-            * np.exp(-np.power(x, 2) / (2 * self.sigma**2))
-        )
-        return kernel / np.sum(kernel)
-
-    def _smooth(self, y: np.ndarray) -> np.ndarray:
-        """Smooth coefficients using a gaussian kernel.
+    @staticmethod
+    def compute_kernel(
+        sigma: float,
+        mu: torch.Tensor,
+        window_radius: int,
+    ) -> torch.Tensor:
+        """Compute the gaussian kernel.
 
         Args:
-            y (np.ndarray): tensor to smooth.
+            sigma (float): Standard deviation.
+            mu (torch.Tensor): Expected value.
+            window_radius (int): Window radius.
 
         Returns:
-            np.ndarray: Smoothed tensor.
+            torch.Tensor: Gaussian kernel.
         """
-        # Padding
-        i_left = self._kernel.shape[0] - 1
-        i_right = self._kernel.shape[0] - 1
-        pad_width = (i_left, i_right)
-        y_padded = np.pad(y, pad_width=pad_width, mode="edge")
-        # Convolution
-        convolved = signal.convolve(y_padded, self._kernel, mode="same")
-        return convolved[i_left:-i_right]
-
-
-class GaussianFilter2D(GaussianFilterBase):
-    """1D Gaussian Filtering."""
-
-    def _generate_kernel(self) -> np.ndarray:
-        """Generate gaussian kernel.
-
-        Returns:
-            np.ndarray: gaussian kernel.
-        """
-        x_cord = np.linspace(
-            -1,
-            1,
-            self._radius * 2,
+        x = torch.arange(
+            -window_radius,
+            window_radius + 1,
+            device=DEVICE.get(),
+            dtype=torch.float64,
         )
-        x_grid = np.repeat(x_cord, self._radius * 2).reshape(
-            (self._radius * 2, self._radius * 2),
-        )
-        y_grid = x_grid.transpose()
-        xy_grid = np.stack([x_grid, y_grid], axis=-1)
+        r = x - mu
+        kernel = torch.exp(-(r**2 / (2 * sigma**2)))
+        kernel /= torch.sum(kernel)
+        return kernel
 
-        # Calculate the 2-dimensional gaussian kernel which is
-        # the product of two gaussian distributions for two different
-        # variables (in this case called x and y)
-        kernel = (1.0 / (self._sigma**2 * 2 * np.pi)) * np.exp(
-            -np.sum((xy_grid) ** 2.0, axis=-1) / (2 * self._sigma**2),
-        )
-        # Make sure sum of values in gaussian kernel equals 1.
-        return kernel / np.sum(kernel)
 
-    def _smooth(self, y: np.ndarray) -> np.ndarray:
-        """Smooth coefficients using a gaussian kernel.
+class GaussianFilter2D(GaussianFilter):
+    """2D Gaussian Filter."""
+
+    def _set_null_mu(self) -> torch.Tensor:
+        return torch.zeros(
+            (2, 1, 1),
+            dtype=torch.float64,
+            device=DEVICE.get(),
+        )
+
+    @staticmethod
+    def compute_kernel(
+        sigma: float,
+        mu: torch.Tensor,
+        window_radius: int,
+    ) -> torch.Tensor:
+        """Compute the gaussian kernel.
 
         Args:
-            y (np.ndarray): tensor to smooth.
+            sigma (float): Standard deviation.
+            mu (torch.Tensor): Expected value.
+            window_radius (int): Window radius.
 
         Returns:
-            np.ndarray: Smoothed tensor.
+            torch.Tensor: Gaussian kernel.
         """
-        # Padding
-        i_left = self._kernel.shape[0] - 1
-        i_right = self._kernel.shape[0] - 1
-        j_left = self._kernel.shape[0] - 1
-        j_right = self._kernel.shape[0] - 1
-        pad_width = ((i_left, i_right), (j_left, j_right))
-        y_padded = np.pad(y, pad_width=pad_width, mode="edge")
-        # Convolution
-        convolved = signal.convolve2d(y_padded, self._kernel, mode="same")
-        return convolved[i_left:-i_right, j_left:-j_right]
+        x = torch.arange(
+            -window_radius,
+            window_radius + 1,
+            device=DEVICE.get(),
+            dtype=torch.float64,
+        )
+        r = torch.norm(torch.stack(torch.meshgrid(x, x)) - mu, dim=0)
+
+        kernel = torch.exp(-(r**2 / (2 * sigma**2)))
+        kernel /= torch.sum(kernel)
+        return kernel
