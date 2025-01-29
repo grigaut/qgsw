18e2ede511dd211da68125b4bc83ec8e93c93afb
grigaut <gaetanrigaut.s@gmail.com>
2025-01-29 17:38:15 +0100

    Use 2 1D convolution for filtering

--- tests/test_filters/test_gaussian.py
+++ tests/test_filters/test_gaussian.py
@@ -7,7 +7,6 @@ from scipy import signal
 
 from qgsw.filters.base import _Filter
 from qgsw.filters.gaussian import (
-    GaussianFilter,
     GaussianFilter1D,
     GaussianFilter2D,
 )
@@ -15,22 +14,27 @@ from qgsw.filters.high_pass import GaussianHighPass1D, GaussianHighPass2D
 from qgsw.filters.low_pass import GaussianLowPass1D, GaussianLowPass2D
 from qgsw.specs import DEVICE
 
-testdata = [
-    pytest.param((500,), GaussianFilter1D(10), id="1D"),
-    pytest.param((500, 500), GaussianFilter2D(10), id="2D"),
-]
 
-
-@pytest.mark.parametrize(("input_shape", "filt"), testdata)
-def test_gaussian_filter(
-    input_shape: tuple[int, ...],
-    filt: GaussianFilter,
-) -> None:
+def test_gaussian_filter_1d() -> None:
     """Test 1D Gaussian filter."""
-    y = torch.rand(input_shape, dtype=torch.float64, device=DEVICE.get())
-
+    y = torch.rand((50,), dtype=torch.float64, device=DEVICE.get())
+    filt = GaussianFilter1D.from_span(1)
     y_filt = filt(y).cpu().numpy()
     y_filt_ref = signal.convolve(
+        y.cpu().numpy(),
+        filt.kernel.cpu().numpy(),
+        mode="same",
+    )
+    assert y_filt.shape == y.shape
+    assert np.isclose(y_filt, y_filt_ref).all()
+
+
+def test_gaussian_filter_2d() -> None:
+    """Test 1D Gaussian filter."""
+    y = torch.rand((50, 50), dtype=torch.float64, device=DEVICE.get())
+    filt = GaussianFilter2D.from_span(1)
+    y_filt = filt(y).cpu().numpy()
+    y_filt_ref = signal.convolve2d(
         y.cpu().numpy(),
         filt.kernel.cpu().numpy(),
         mode="same",
