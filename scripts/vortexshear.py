bdeffc1153f9596b5a91389186fd48598a7bf8d1
grigaut <gaetanrigaut.s@gmail.com>
2025-02-04 15:02:32 +0100

    Add comments to script

--- scripts/vortexshear.py
+++ scripts/vortexshear.py
@@ -23,12 +23,10 @@ dtype = torch.float64
 
 def grad_perp(f, dx, dy):
     """Orthogonal gradient"""
-    return (f[..., :-1] - f[..., 1:]) / dy, (
-        f[..., 1:, :] - f[..., :-1, :]
-    ) / dx
+    return (f[..., :-1] - f[..., 1:]) / dy, (f[..., 1:, :] - f[..., :-1, :]) / dx
 
 
-# grid
+## Space discretization definition -------------------------------------------
 nx = 192
 ny = 192
 nl = 1
@@ -42,7 +40,26 @@ x_, y_ = (
     torch.linspace(-Ly / 2, Ly / 2, ny + 1, dtype=dtype, device=device),
 )
 x, y = torch.meshgrid(x_, y_, indexing="ij")
+## ---------------------------------------------------------------------------
 
+## Layers Reference thickness definition -------------------------------------
+# H = torch.zeros(nl, 1, 1, dtype=dtype, device=device)
+# if nl == 1:
+#     H[0, 0, 0] = 1000  # 1km
+# 1-layer reduced gravity
+H = torch.tensor([[[400]]], dtype=dtype, device=device)
+## ---------------------------------------------------------------------------
+
+## Reduced Gravity definition ------------------------------------------------
+# g_prime = torch.zeros(nl, 1, 1, dtype=dtype, device=device)
+# if nl == 1:
+#     g_prime[0, 0, 0] = 10.0
+# 1-layer reduced gravity
+g_prime = torch.tensor([[[0.05]]], dtype=dtype, device=device)
+## ---------------------------------------------------------------------------
+
+
+## Circular Mask definition --------------------------------------------------
 xc_ = 0.5 * (x_[1:] + x_[:-1])
 yc_ = 0.5 * (y_[1:] + y_[:-1])
 xc, yc = torch.meshgrid(xc_, yc_, indexing="ij")
@@ -50,16 +67,11 @@ rc = torch.sqrt(xc**2 + yc**2)
 # circular domain mask
 apply_mask = False
 mask = (rc < L / 2).type(torch.float64) if apply_mask else torch.ones_like(xc)
+## ---------------------------------------------------------------------------
 
-H = torch.zeros(nl, 1, 1, dtype=dtype, device=device)
-if nl == 1:
-    H[0, 0, 0] = 1000  # 1km
 
 # density/gravity
 rho = 1000.0
-g_prime = torch.zeros(nl, 1, 1, dtype=dtype, device=device)
-if nl == 1:
-    g_prime[0, 0, 0] = 10.0
 
 flip_sign = False
 # Burger and Rossby
@@ -79,36 +91,14 @@ for Bu, Ro in [
     beta = 0
     f = f0 + beta * (y - Ly / 2)
 
-    z = x + 1j * y
-    theta = torch.angle(z)
-    r = torch.sqrt(x**2 + y**2)
-
-    # create rankine vortex with tripolar perturbation
-    epsilon = 1e-3
-    r *= 1 + epsilon * torch.cos(theta * 3)
-    soft_step = lambda x: torch.sigmoid(x / 100)
-    mask_core = soft_step(r0 - r)
-    mask_ring = soft_step(r - r1) * soft_step(r2 - r)
-    vor = 1.0 * (-mask_core / mask_core.mean() + mask_ring / mask_ring.mean())
-    if flip_sign:
-        vor *= -1
-    laplace_dstI = compute_laplace_dstI(
-        nx, ny, dx, dy, {"device": device, "dtype": dtype}
-    )
-    psi_hat = dstI2D(vor[1:-1, 1:-1]) / laplace_dstI
-    psi = F.pad(dstI2D(psi_hat), (1, 1, 1, 1)).unsqueeze(0).unsqueeze(0)
-
-    # set psi amplitude to have correct Rossby number
-    u, v = grad_perp(psi, dx, dy)
-    u_norm_max = max(torch.abs(u).max(), torch.abs(v).max())
-    psi *= Ro * f0 * r0 / u_norm_max
-    p_init = psi * f0
-
     # wind forcing, bottom drag
     taux = 0.0
     tauy = 0.0
     bottom_drag_coef = 0.0
 
+    ## Model Parameters Definition -------------------------------------------
+
+    ### QG Model
     param_qg = {
         "nx": nx,
         "ny": ny,
@@ -126,9 +116,13 @@ for Bu, Ro in [
         "mask": mask,
         "compile": True,
         "slip_coef": 1.0,
+        # dt will be modified later after computing CFL condition
+        # yet the model must be instantiated for the conversion between
+        # pressure and U,V,H to be done and CFL condition to be computed
         "dt": 0.0,
     }
 
+    ### SW Model
     param_sw = {
         "nx": nx,
         "ny": ny,
@@ -151,10 +145,41 @@ for Bu, Ro in [
         "barotropic_filter_spectral": False,
         "dt": 0.0,  # time-step (s)
     }
+    ## -----------------------------------------------------------------------
 
+    ## Create QG model -------------------------------------------------------
     qg_multilayer = QG(param_qg)
+    ## -----------------------------------------------------------------------
 
+    ## Initial Perturbation --------------------------------------------------
+    # create rankine vortex with tripolar perturbation
+    z = x + 1j * y
+    theta = torch.angle(z)
+    r = torch.sqrt(x**2 + y**2)
+    epsilon = 1e-3
+    r *= 1 + epsilon * torch.cos(theta * 3)
+    soft_step = lambda x: torch.sigmoid(x / 100)
+    mask_core = soft_step(r0 - r)
+    mask_ring = soft_step(r - r1) * soft_step(r2 - r)
+    vor = 1.0 * (-mask_core / mask_core.mean() + mask_ring / mask_ring.mean())
+    if flip_sign:
+        vor *= -1
+    laplace_dstI = compute_laplace_dstI(
+        nx, ny, dx, dy, {"device": device, "dtype": dtype}
+    )
+    psi_hat = dstI2D(vor[1:-1, 1:-1]) / laplace_dstI
+    psi = F.pad(dstI2D(psi_hat), (1, 1, 1, 1)).unsqueeze(0).unsqueeze(0)
+
+    # set psi amplitude to have correct Rossby number
+    u, v = grad_perp(psi, dx, dy)
+    u_norm_max = max(torch.abs(u).max(), torch.abs(v).max())
+    psi *= Ro * f0 * r0 / u_norm_max
+    p_init = psi * f0
+    # Use G to convert pressure to u,v and h to initialize the model
+    # This is why the QG model must be instantiated before the initial
+    # conditions are defined, to provide the "pressure to U,V,H" function G
     u_init, v_init, h_init = qg_multilayer.G(p_init)
+    ## -----------------------------------------------------------------------
 
     u_max, v_max, c = (
         torch.abs(u_init).max().item() / dx,
@@ -164,23 +189,48 @@ for Bu, Ro in [
     print(f"u_max {u_max:.2e}, v_max {v_max:.2e}, c {c:.2e}")
     cfl_adv = 0.5
     cfl_gravity = 5 if param_sw["barotropic_filter"] else 0.5
-    dt = min(cfl_adv * dx / u_max, cfl_adv * dy / v_max, cfl_gravity * dx / c)
 
+    ## Compute dt from U,V and H using the CFL condition ---------------------
+    dt = min(cfl_adv * dx / u_max, cfl_adv * dy / v_max, cfl_gravity * dx / c)
+    ## -----------------------------------------------------------------------
+
+    ## Set Time step ---------------------------------------------------------
     qg_multilayer.dt = dt
+    ## -----------------------------------------------------------------------
+
+    ## Set initial U, V and H ------------------------------------------------
     qg_multilayer.u = torch.clone(u_init)
     qg_multilayer.v = torch.clone(v_init)
     qg_multilayer.h = torch.clone(h_init)
+    ## -----------------------------------------------------------------------
+
+    ## Compute diagnostic variable using the new U, V and H ------------------
+    # This is required before performing a model step to ensure that
+    # diagnostic variables used for advection are updated
     qg_multilayer.compute_diagnostic_variables()
+    ## -----------------------------------------------------------------------
 
     w_qg = (qg_multilayer.omega.squeeze() / qg_multilayer.area).cpu().numpy()
 
-    # update time step
+    ## Set Time step for SW Parameters ---------------------------------------
     param_sw["dt"] = dt
+    ## -----------------------------------------------------------------------
+
+    ## Create SW model -------------------------------------------------------
     sw_multilayer = SW(param_sw)
+    ## -----------------------------------------------------------------------
+
+    ## Set initial U, V and H ------------------------------------------------
     sw_multilayer.u = torch.clone(u_init)
     sw_multilayer.v = torch.clone(v_init)
     sw_multilayer.h = torch.clone(h_init)
+    ## -----------------------------------------------------------------------
+
+    ## Compute diagnostic variable using the new U, V and H ------------------
+    # This is required before performing a model step to ensure that
+    # diagnostic variables used for advection are updated
     sw_multilayer.compute_diagnostic_variables()
+    ## -----------------------------------------------------------------------
 
     # time params
     t = 0
@@ -190,7 +240,7 @@ for Bu, Ro in [
 
     w_0 = qg_multilayer.omega.squeeze() / qg_multilayer.dx / qg_multilayer.dy
     tau = 1.0 / torch.sqrt(w_0.pow(2).mean()).cpu().item()
-    print(f"tau = {tau *f0:.2f} f0-1")
+    print(f"tau = {tau * f0:.2f} f0-1")
 
     t_end = 8 * tau
     freq_plot = int(t_end / 100 / dt) + 1
@@ -235,27 +285,25 @@ for Bu, Ro in [
             )
             a[0].imshow(np.ma.masked_where(mask_w, w_qg[0, 0]).T, **kwargs)
             a[1].imshow(np.ma.masked_where(mask_w, w_sw[0, 0]).T, **kwargs)
-            a[2].imshow(
-                np.ma.masked_where(mask_w, (w_qg - w_sw)[0, 0]).T, **kwargs
-            )
+            a[2].imshow(np.ma.masked_where(mask_w, (w_qg - w_sw)[0, 0]).T, **kwargs)
             f.suptitle(
-                f'Ro={Ro:.2f}, Bu={Bu:.2f}, t={t/tau:.2f}$\\tau$, '
-                f'{"neg." if flip_sign else "pos"} $f_0$'
+                f"Ro={Ro:.2f}, Bu={Bu:.2f}, t={t / tau:.2f}$\\tau$, "
+                f"{'neg.' if flip_sign else 'pos'} $f_0$"
             )
             plt.pause(0.05)
 
+        ## Model step --------------------------------------------------------
+        ### QG
         qg_multilayer.step()
+        ### SW
         sw_multilayer.step()
+        ## -------------------------------------------------------------------
         t += dt
         if n % freq_checknan == 0:
             if torch.isnan(qg_multilayer.h).any():
-                raise ValueError(
-                    f"Stopping, NAN number in QG h at iteration {n}."
-                )
+                raise ValueError(f"Stopping, NAN number in QG h at iteration {n}.")
             if torch.isnan(sw_multilayer.h).any():
-                raise ValueError(
-                    f"Stopping, NAN number in SW h at iteration {n}."
-                )
+                raise ValueError(f"Stopping, NAN number in SW h at iteration {n}.")
 
         if freq_log > 0 and n % freq_log == 0:
             print(f"n={n:05d}, {qg_multilayer.get_print_info()}")
