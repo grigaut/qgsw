efc4184cb7917bf772d0652989885e3eb3d62f7a
grigaut <gaetanrigaut.s@gmail.com>
2025-04-04 12:01:36 +0200

    Add offset to variable computation

--- src/qgsw/models/qg/projected/modified/filtered/variables.py
+++ src/qgsw/models/qg/projected/modified/filtered/variables.py
@@ -1,15 +1,21 @@
 """Variables."""
 
-from torch._tensor import Tensor
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import torch
 
 from qgsw.fields.variables.dynamics import (
     Psi2,
     StreamFunctionFromVorticity,
 )
-from qgsw.fields.variables.prognostic_tuples import (
-    UVHTAlpha,
-)
-from qgsw.filters.base import _Filter
+
+if TYPE_CHECKING:
+    from qgsw.fields.variables.prognostic_tuples import (
+        UVHTAlpha,
+    )
+    from qgsw.filters.base import _Filter
 
 
 class CollinearFilteredPsi2(Psi2):
@@ -19,17 +25,25 @@ class CollinearFilteredPsi2(Psi2):
         self,
         psi_vort: StreamFunctionFromVorticity,
         filt: _Filter,
+        offset_psi1: torch.Tensor | None = None,
+        offset_psi2: torch.Tensor | None = None,
     ) -> None:
         """Instantiate the variable.
 
         Args:
             psi_vort (StreamFunctionFromVorticity): Stream Function.
             filt (_Filter): Filter to apply to top stream function.
+            offset_psi1 (torch.Tensor | None): Offset to apply to top
+                stream function.
+            offset_psi2 (torch.Tensor | None): Offset to apply to
+                bottom stream function.
         """
         super().__init__(psi_vort)
         self._filter = filt
+        self._offset_psi1 = offset_psi1
+        self._offset_psi2 = offset_psi2
 
-    def _compute(self, prognostic: UVHTAlpha) -> Tensor:
+    def _compute(self, prognostic: UVHTAlpha) -> torch.Tensor:
         """Compute the variable value.
 
         Args:
@@ -46,5 +60,17 @@ class CollinearFilteredPsi2(Psi2):
                 └── (n_ens, 1, nx, ny)-shaped
         """
         psi1 = self._psi_vort.compute_no_slice(prognostic)[:, :1]
-        psi_filt = self._filter(psi1[0, 0]).unsqueeze(0).unsqueeze(0)
-        return prognostic.alpha * psi_filt
+        if self._offset_psi1 is None:
+            offset_psi1 = torch.zeros_like(psi1)
+        else:
+            offset_psi1 = self._offset_psi1
+        if self._offset_psi2 is None:
+            offset_psi2 = torch.zeros_like(psi1)
+        else:
+            offset_psi2 = self._offset_psi2
+        psi_filt = (
+            self._filter(psi1[0, 0] - offset_psi1[0, 0])
+            .unsqueeze(0)
+            .unsqueeze(0)
+        )
+        return prognostic.alpha * psi_filt + offset_psi2
