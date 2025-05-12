ecd429249a36bb303b0fc520df6038803deea05d
grigaut <gaetanrigaut.s@gmail.com>
2025-05-12 16:35:29 +0200

    MOdify config to comply with python 13.10

--- src/qgsw/configs/models.py
+++ src/qgsw/configs/models.py
@@ -97,7 +97,7 @@ class ModelConfig(
     """Model configuration."""
 
     prefix: str
-    name: str | None = None
+    name: Union[str, None] = None
     layers: list[PositiveFloat]
     reduced_gravity: list[PositiveFloat]
     collinearity_coef: Union[CoefConfigVar, None] = Field(
