613bb44ae989088f6e29721cee704ccb8dc1b063
grigaut <gaetanrigaut.s@gmail.com>
2025-05-13 15:05:23 +0200

    Use reference with synchronization

--- src/qgsw/models/synchronization/sync.py
+++ src/qgsw/models/synchronization/sync.py
@@ -2,10 +2,13 @@
 
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Generic, TypeVar
 
 from qgsw import verbose
+from qgsw.models.base import ModelUVH
+from qgsw.models.references.base import Reference
 from qgsw.models.synchronization.initial_conditions import InitialCondition
 
 if TYPE_CHECKING:
@@ -15,61 +18,49 @@ if TYPE_CHECKING:
     from qgsw.configs.physics import PhysicsConfig
     from qgsw.configs.space import SpaceConfig
     from qgsw.fields.variables.prognostic_tuples import UVH
-    from qgsw.models.base import ModelUVH
     from qgsw.models.names import ModelCategory
     from qgsw.models.qg.uvh.projectors.core import QGProjector
 
+T = TypeVar("T")
 
-class Synchronizer:
+
+class _Synchronizer(Generic[T], ABC):
+    __slots__ = ("_ic", "_model", "_ref")
+
+    def __init__(self, reference: T, model: ModelUVH) -> None:
+        self._ref = reference
+        self._model = model
+        self._ic = InitialCondition(self._model)
+
+    @abstractmethod
+    def __call__(self) -> None:
+        """Rescale if necessary and synchronize model to ref."""
+
+
+class ModelSynchronizer(_Synchronizer[ModelUVH]):
     """Model synchronizer.
 
     Synchronizes model by using the 'input' model as initial condition
     for the 'output' model.
     """
 
-    __slots__ = (
-        "_m_in",
-        "_m_out",
-        "_model_sync",
-        "_nxin",
-        "_nxout",
-        "_nyin",
-        "_nyout",
-        "_syncout",
-    )
-
-    def __init__(self, model_in: ModelUVH, model_out: ModelUVH) -> None:
-        """Instantiate model synchronizer.
-
-        Args:
-            model_in (ModelUVH): Model to use as reference.
-            model_out (ModelUVH): Model to synchronize with reference.
-        """
-        # IN specs
-        self._m_in = model_in
-        self._nxin, self._nyin = model_in.space.nx, model_in.space.ny
-        # OUT specs
-        self._m_out = model_out
-        self._nxout, self._nyout = model_out.space.nx, model_out.space.ny
-        self._syncout = InitialCondition(model_out)
-
     def __call__(self) -> None:
-        """Rescale if necessary and Synchronize models."""
+        """Rescale if necessary and synchronize model to ref."""
         verbose.display(
             msg="Synchronizing model states...",
             trigger_level=1,
         )
-        self._syncout.set_initial_condition(
-            self._m_in.prognostic.uvh,
-            self._m_in.P,
-            self._m_in.space.dx,
-            self._m_in.space.dy,
-            self._m_in.get_category(),
+        self._ic.set_initial_condition(
+            self._ref.prognostic.uvh,
+            self._ref.P,
+            self._ref.space.dx,
+            self._ref.space.dy,
+            self._ref.get_category(),
         )
         verbose.display(
             msg=(
-                f"'{self._m_out.name}' model state now "
-                f"matches '{self._m_in.name}' state."
+                f"'{self._model.name}' model state now "
+                f"matches '{self._ref.name}' state."
             ),
             trigger_level=1,
         )
@@ -101,7 +92,7 @@ class Synchronizer:
                 initial condition, hence the category of the model
                 which computed uvh.
         """
-        syncin = InitialCondition(self._m_in)
+        syncin = InitialCondition(self._ref)
         syncin.set_initial_condition(
             uvh,
             qg_proj,
@@ -121,7 +112,7 @@ class Synchronizer:
         dtype: torch.dtype | None = None,
         device: torch.device | None = None,
     ) -> None:
-        """Set initial condition from file.
+        """Set initial condition from file for both models.
 
         Args:
             file (str | Path): File to use as UVH input.
@@ -134,7 +125,7 @@ class Synchronizer:
             dtype (torch.dtype | None, optional): Dtype. Defaults to None.
             device (torch.device | None, optional): Device. Defaults to None.
         """
-        syncin = InitialCondition(self._m_in)
+        syncin = InitialCondition(self._ref)
         syncin.set_initial_condition_from_file(
             Path(file),
             space_config=space_config,
@@ -144,3 +135,20 @@ class Synchronizer:
             device=device,
         )
         self()
+
+
+class Synchronizer(_Synchronizer[Reference]):
+    """Synchronizer."""
+
+    def __call__(self) -> None:
+        """Rescale if necessary and synchronize model to ref."""
+        self._ref.at_time(self._model.time.item())
+        prognostic = self._ref.load()
+        dx, dy = self._ref.retrieve_dxdy()
+        self._ic.set_initial_condition(
+            prognostic.uvh,
+            self._ref.retrieve_P(),
+            dx,
+            dy,
+            self._ref.retrieve_category(),
+        )
