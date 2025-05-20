ebef71dc2342dbefdabf2a59ef0b8b7fd34e5696
grigaut <gaetanrigaut.s@gmail.com>
2025-05-20 11:16:58 +0200

    Add __name for models

--- src/qgsw/models/qg/uvh/modified/filtered/core.py
+++ src/qgsw/models/qg/uvh/modified/filtered/core.py
@@ -64,6 +64,8 @@ class QGCollinearFilteredSF(QGAlpha[CollinearFilteredQGProjector]):
             msg=f"Creating {self.__class__.__name__} model...",
             trigger_level=1,
         )
+        self.__instance_nb = next(self._instance_count)
+        self.__name = f"{self.__class__.__name__}-{self.__instance_nb}"
         ModelParamChecker.__init__(
             self,
             space_2d=space_2d,
