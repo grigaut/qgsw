1772fb5aca20b9abed6c7c6179928c25be6820bb
grigaut <gaetanrigaut.s@gmail.com>
2025-08-26 15:16:54 +0200

    plt wrapper in plots

---
+++ src/qgsw/plots/plt_wrapper.py
@@ -0,0 +1,40 @@
+"""Wrapper for some of matplotlib.pyplot's functions."""
+
+from __future__ import annotations
+
+from typing import ParamSpec
+
+import numpy as np
+import torch
+from matplotlib import pyplot as plt
+
+Param = ParamSpec("Param")
+
+
+def imshow(
+    data: torch.Tensor | np.ndarray,
+    *,
+    ax: plt.Axes | None = None,
+    title: str | None = None,
+    **kwargs: Param.kwargs,
+) -> None:
+    """Wrapper for plt.imshow.
+
+    Args:
+        data (torch.Tensor | np.ndarray): 2D array to plot.
+        ax (plt.Axes | None, optional): Axes to plot on. Defaults to None.
+        title (str | None, optional): Title. Defaults to None.
+        **kwargs: optional arguments to pass to plt.imshow.
+    """
+    if isinstance(data, torch.Tensor):
+        data = data.cpu().numpy()
+    kwargs.setdefault("vmax", np.max(np.abs(data)))
+    kwargs.setdefault("vmin", -kwargs["vmax"])
+    kwargs.setdefault("cmap", "RdBu_r")
+    if ax is None:
+        ax = plt.subplot()
+
+    cbar = ax.imshow(data.T, **kwargs)
+    ax.figure.colorbar(cbar, ax=ax)
+    if title is not None:
+        ax.set_title(title)
