a23bb6b973c1925bef30fb19271bbfe50c91a8d8
grigaut <gaetanrigaut.s@gmail.com>
2025-10-10 11:39:17 +0200

    Implemenetd restore method

--- src/qgsw/models/qg/psiq/optim/utils.py
+++ src/qgsw/models/qg/psiq/optim/utils.py
@@ -2,6 +2,10 @@
 
 import torch
 
+from qgsw import logging
+
+logger = logging.getLogger(__name__)
+
 
 class EarlyStop:
     """Early stop."""
@@ -32,7 +36,7 @@ class EarlyStop:
             self.previous_loss = loss.detach()
             return False
         loss_ = self.previous_loss
-        if ((loss - loss_).abs() / loss_.abs()) < self.eps:
+        if ((loss - loss_).abs() / (loss_.abs() + 1e-12)) < self.eps:
             self.counter += 1
         else:
             self.counter = 0
@@ -55,10 +59,21 @@ class RegisterParams:
             loss (torch.Tensor): Loss value.
             **kwargs (torch.Tensor): Parameters.
         """
-        if loss > self.best_loss:
+        if loss.item() > self.best_loss.item():
             return
-        self.params = {e: kwargs[e].detach() for e in kwargs}
+        self.params = {k: v.detach().clone() for k, v in kwargs.items()}
         self.best_loss = torch.clone(loss.detach())
+
+    def restore(self, **targets: torch.Tensor) -> None:
+        """Restore best parameters."""
+        for k, t in targets.items():
+            if k in self.params:
+                t.data.copy_(self.params[k])
+                msg = (
+                    f"Restored {k} from iteration with "
+                    f"loss: {self.best_loss:.5f}"
+                )
+                logger.info(msg)
 
     def __repr__(self) -> str:
         """Implement __repr__."""
