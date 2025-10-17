36e4a695785be2fabb2c5010060d9b8187ca2c76
grigaut <gaetanrigaut.s@gmail.com>
2025-10-17 14:45:02 +0200

    Add vorticity to rmse

---
+++ scripts/variational_analysis_psi2_reg_vort.py
@@ -0,0 +1,477 @@
+"""Variational analysis."""
+
+from __future__ import annotations
+
+import argparse
+import pathlib
+from dataclasses import dataclass
+from pathlib import Path
+
+import torch
+
+from qgsw import logging
+from qgsw.configs.core import Configuration
+from qgsw.fields.variables.tuples import UVH
+from qgsw.forcing.wind import WindForcing
+from qgsw.logging import getLogger, setup_root_logger
+from qgsw.logging.utils import box, step
+from qgsw.masks import Masks
+from qgsw.models.core.flux import div_flux_5pts_only
+from qgsw.models.qg.psiq.core import QGPSIQ
+from qgsw.models.qg.psiq.filtered.core import (
+    QGPSIQCollinearSF,
+    QGPSIQFixeddSF2,
+)
+from qgsw.models.qg.psiq.optim.utils import EarlyStop, RegisterParams
+from qgsw.models.qg.stretching_matrix import compute_A
+from qgsw.models.qg.uvh.projectors.core import QGProjector
+from qgsw.solver.boundary_conditions.base import Boundaries
+from qgsw.solver.finite_diff import grad_perp, laplacian
+from qgsw.spatial.core.discretization import (
+    SpaceDiscretization2D,
+    SpaceDiscretization3D,
+)
+from qgsw.spatial.core.grid_conversion import interpolate
+from qgsw.specs import defaults
+from qgsw.utils import covphys
+from qgsw.utils.interpolation import QuadraticInterpolation
+
+try:
+    from typing import Self
+except ImportError:
+    from typing_extensions import Self
+torch.backends.cudnn.deterministic = True
+torch.set_grad_enabled(False)
+
+## Config
+
+
+@dataclass
+class ScriptArgs:
+    """Script arguments."""
+
+    config: Path
+    verbose: int
+    indices: list[int]
+
+    @classmethod
+    def from_cli(cls) -> Self:
+        """Instantiate script arguments from CLI.
+
+        Args:
+            default_config (str): Default configuration path.
+
+        Returns:
+            Self: ScriptArgs.
+        """
+        parser = argparse.ArgumentParser(
+            description="Retrieve script arguments.",
+        )
+        parser.add_argument(
+            "--config",
+            required=True,
+            type=pathlib.Path,
+            help="Configuration File Path (from qgsw root level)",
+        )
+        parser.add_argument(
+            "-v",
+            "--verbose",
+            action="count",
+            default=0,
+            help="Verbose level.",
+        )
+        parser.add_argument(
+            "-i",
+            "--indices",
+            required=True,
+            nargs="+",
+            type=int,
+            help="Indices (imin, imax, jmin, jmax), "
+            "for example (64, 128, 128, 256).",
+        )
+        return cls(**vars(parser.parse_args()))
+
+
+args = ScriptArgs.from_cli()
+specs = defaults.get()
+
+setup_root_logger(args.verbose)
+logger = getLogger(__name__)
+
+ROOT_PATH = Path(__file__).parent.parent
+config = Configuration.from_toml(ROOT_PATH.joinpath(args.config))
+
+output_dir = config.io.output.directory
+
+
+# Simulation parameters
+
+dt = 7200
+optim_max_step = 200
+n_steps_per_cyle = 250
+comparison_interval = 1
+n_cycles = 3
+msg = (
+    f"Performing {n_cycles} cycles of {n_steps_per_cyle} "
+    f"steps with up to {optim_max_step} optimization steps."
+)
+logger.info(box(msg, style="="))
+
+# Parameters
+
+H = config.model.h
+g_prime = config.model.g_prime
+H1, H2 = H[0], H[1]
+g1, g2 = g_prime[0], g_prime[1]
+beta_plane = config.physics.beta_plane
+bottom_drag_coef = config.physics.bottom_drag_coefficient
+slip_coef = config.physics.slip_coef
+
+space = SpaceDiscretization3D.from_config(
+    config.space,
+    config.model,
+)
+P = QGProjector(
+    A=compute_A(H=H, g_prime=g_prime),
+    H=H.unsqueeze(-1).unsqueeze(-1),
+    space=space,
+    f0=beta_plane.f0,
+    masks=Masks.empty(
+        nx=config.space.nx,
+        ny=config.space.ny,
+    ),
+)
+dx, dy = space.dx, space.dy
+nx, ny = space.nx, space.ny
+
+wind = WindForcing.from_config(
+    config.windstress,
+    config.space,
+    config.physics,
+)
+tx, ty = wind.compute()
+
+uvh0 = UVH.from_file(config.simulation.startup.file)
+
+U: float = (uvh0.u[0, 0].max() ** 2 + uvh0.v[0, 0].max() ** 2).sqrt().item()
+L: float = dx.item()
+T: float = L / U
+
+psi_start = P.compute_p(covphys.to_cov(uvh0, dx, dy))[0] / beta_plane.f0
+
+## Areas
+
+indices = args.indices
+imin, imax, jmin, jmax = indices
+msg = f"Focusing on i in [{imin}, {imax}] and j in [{jmin}, {jmax}]"
+logger.info(msg)
+
+p = 4
+psi_slices = [slice(imin, imax + 1), slice(jmin, jmax + 1)]
+psi_slices_w = [slice(imin - p, imax + p + 1), slice(jmin - p, jmax + p + 1)]
+
+## Error
+
+
+def rmse(psi: torch.Tensor, psi_ref: torch.Tensor) -> float:
+    """RMSE."""
+    return ((psi - psi_ref) / U / L).square().mean()
+
+
+def rmse_vort(psi: torch.Tensor, psi_ref: torch.Tensor) -> float:
+    """RMSE."""
+    v = laplacian(psi, dx, dy)
+    v_ref = laplacian(psi_ref, dx, dy)
+    return ((v - v_ref) / U * L).square().mean()
+
+
+theta = 200
+
+
+def regularization(
+    psi1: torch.Tensor,
+    psi2: torch.Tensor,
+    dpsi1: torch.Tensor,
+    dpsi2: torch.Tensor,
+) -> torch.Tensor:
+    """Compute regularization.
+
+    Args:
+        psi1 (torch.Tensor): Top layer stream function.
+        psi2 (torch.Tensor): Bottom layer stream function.
+        dpsi1 (torch.Tensor): Top layer stream function derivative.
+        dpsi2 (torch.Tensor): Bottom layer stream function derivative
+
+    Returns:
+        torch.Tensor: ||∂_t q₂ + J(ѱ₂,q₂)||² (normalized by U / LT)
+    """
+    dtq2 = (
+        laplacian(dpsi2, dx, dy)
+        - beta_plane.f0**2 / H2 / g2 * (dpsi2 - dpsi1[:, :1])[..., 1:-1, 1:-1]
+    )
+    dtq2 = interpolate(dtq2[..., 3:-3, 3:-3])
+    q2 = (
+        laplacian(psi2, dx, dy)
+        - beta_plane.f0**2 / H2 / g2 * (psi2 - psi1[:, :1])[..., 1:-1, 1:-1]
+    )
+
+    u2, v2 = grad_perp(psi2[..., 4:-4, 4:-4])
+    u2 /= dx
+    v2 /= dy
+
+    dq_2 = div_flux_5pts_only(interpolate(q2), u2, v2, dx, dy)
+    return ((dtq2 + dq_2) / U * L * T).square().sum()
+
+
+gamma = 200
+
+# PV computation
+
+
+def compute_q_alpha(psi: torch.Tensor, alpha: torch.Tensor) -> torch.Tensor:
+    """Compute pv using alpha."""
+    return interpolate(
+        laplacian(psi, dx, dy)
+        - beta_plane.f0**2 * (1 / H1 / g1 + 1 / H1 / g1) * psi[..., 1:-1, 1:-1]
+        + beta_plane.f0**2 * (1 / H1 / g2) * alpha * psi[..., 1:-1, 1:-1]
+    )
+
+
+def compute_q_psi2(psi: torch.Tensor, psi2: torch.Tensor) -> torch.Tensor:
+    """Compute pv using psi2."""
+    return interpolate(
+        laplacian(psi, dx, dy)
+        - beta_plane.f0**2 * (1 / H1 / g1 + 1 / H1 / g1) * psi[..., 1:-1, 1:-1]
+        + beta_plane.f0**2 * (1 / H1 / g2) * psi2[..., 1:-1, 1:-1]
+    )
+
+
+# Models
+## Three Layer model
+
+model_3l = QGPSIQ(
+    space_2d=space.remove_z_h(),
+    H=H,
+    beta_plane=config.physics.beta_plane,
+    g_prime=g_prime,
+)
+model_3l.set_wind_forcing(tx, ty)
+model_3l.masks = Masks.empty_tensor(
+    model_3l.space.nx,
+    model_3l.space.ny,
+    device=specs["device"],
+)
+model_3l.bottom_drag_coef = bottom_drag_coef
+model_3l.slip_coef = slip_coef
+model_3l.dt = dt
+y0 = model_3l.y0
+
+
+## Inhomogeneous models
+def set_inhomogeneous_model(
+    model: QGPSIQ | QGPSIQCollinearSF | QGPSIQFixeddSF2,
+) -> QGPSIQ | QGPSIQCollinearSF | QGPSIQFixeddSF2:
+    """Set up inhomogeneous model."""
+    space = model.space
+    model.y0 = y0
+    model.masks = Masks.empty_tensor(
+        space.nx,
+        space.ny,
+        device=specs["device"],
+    )
+    model.bottom_drag_coef = 0
+    model.wide = True
+    model.slip_coef = slip_coef
+    model.dt = dt
+    return model
+
+
+outputs = []
+model_3l.reset_time()
+model_3l.set_psi(psi_start)
+
+space_slice = SpaceDiscretization2D.from_tensors(
+    x=P.space.remove_z_h().omega.xy.x[imin : imax + 1, 0],
+    y=P.space.remove_z_h().omega.xy.y[0, jmin : jmax + 1],
+)
+
+model_dpsi = QGPSIQFixeddSF2(
+    space_2d=space_slice,
+    H=H[:2],
+    beta_plane=beta_plane,
+    g_prime=g_prime[:2],
+)
+model_dpsi: QGPSIQFixeddSF2 = set_inhomogeneous_model(model_dpsi)
+
+model_dpsi.set_wind_forcing(
+    tx[imin:imax, jmin : jmax + 1], ty[imin : imax + 1, jmin:jmax]
+)
+
+
+def extract_psi_w(psi: torch.Tensor) -> torch.Tensor:
+    """Extract psi."""
+    return psi[..., psi_slices_w[0], psi_slices_w[1]]
+
+
+def extract_psi_bc(psi: torch.Tensor) -> Boundaries:
+    """Extract psi."""
+    return Boundaries.extract(psi, p, -p - 1, p, -p - 1, 2)
+
+
+for c in range(n_cycles):
+    times = [model_3l.time.item()]
+
+    psi0 = extract_psi_w(model_3l.psi[:, :1])
+    psi0_mean = psi0[:, :1].mean()
+    psi_bc = extract_psi_bc(psi0)
+
+    psis = [psi0]
+    psi_bcs = [psi_bc]
+
+    for _ in range(1, n_steps_per_cyle):
+        model_3l.step()
+
+        times.append(model_3l.time.item())
+
+        psi = extract_psi_w(model_3l.psi[:, :1])
+        psi_bc = extract_psi_bc(psi)
+
+        psis.append(psi)
+        psi_bcs.append(psi_bc)
+
+    msg = f"Cycle {step(c + 1, n_cycles)}: Model spin-up completed."
+    logger.info(box(msg, style="round"))
+
+    psi_bc_interp = QuadraticInterpolation(times, psi_bcs)
+
+    psi2 = (torch.rand_like(psi0) * 1e-1).requires_grad_()
+    dpsi2 = (torch.rand_like(psi2) * 1e-3).requires_grad_()
+
+    optimizer = torch.optim.Adam(
+        [{"params": [psi2], "lr": 1e-1}, {"params": [dpsi2], "lr": 1e-3}],
+    )
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, factor=0.5, patience=5
+    )
+    early_stop = EarlyStop()
+    register_params_dpsi2 = RegisterParams()
+
+    for o in range(optim_max_step):
+        optimizer.zero_grad()
+        model_dpsi.reset_time()
+
+        with torch.enable_grad():
+            q0 = compute_q_psi2(psi0, psi2 * psi0_mean)[
+                ..., (p - 1) : -(p - 1), (p - 1) : -(p - 1)
+            ]
+            q_bcs = [
+                Boundaries.extract(
+                    compute_q_psi2(
+                        psi[:, :1], psi2 * psi0_mean + n * dt * dpsi2
+                    ),
+                    p - 2,
+                    -(p - 1),
+                    p - 2,
+                    -(p - 1),
+                    3,
+                )
+                for n, psi in enumerate(psis)
+            ]
+
+            model_dpsi.set_psiq(psi0[:, :1, p:-p, p:-p], q0)
+            q_bc_interp = QuadraticInterpolation(times, q_bcs)
+            model_dpsi.set_boundary_maps(psi_bc_interp, q_bc_interp)
+            model_dpsi.dpsi2 = dpsi2[..., p:-p, p:-p]
+
+            loss = torch.tensor(0, **defaults.get())
+
+            for n in range(1, n_steps_per_cyle):
+                model_dpsi.step()
+
+                if (n + 1) % comparison_interval == 0:
+                    loss += rmse(
+                        model_dpsi.psi[0, 0], psis[n][0, 0, p:-p, p:-p]
+                    )
+                    reg = gamma * regularization(
+                        psis[n - 1],
+                        psi2 + n * dt * dpsi2,
+                        (psis[n] - psis[n - 1]) / dt,
+                        dpsi2,
+                    )
+                    loss += theta * rmse_vort(
+                        model_dpsi.psi[0, 0], psis[n][0, 0, p:-p, p:-p]
+                    )
+                    loss += reg
+
+        if torch.isnan(loss.detach()):
+            msg = "Loss has diverged."
+            logger.warning(box(msg, style="="))
+            break
+
+        register_params_dpsi2.step(
+            loss,
+            psi2=psi2 * psi0_mean,
+            dpsi2=dpsi2,
+        )
+
+        if early_stop.step(loss):
+            msg = f"Convergence reached after {o + 1} iterations."
+            logger.info(msg)
+            break
+
+        loss_ = loss.cpu().item()
+
+        msg = (
+            f"Cycle {step(c + 1, n_cycles)} | "
+            f"dѱ₂ optimization step {step(o + 1, optim_max_step)} | "
+            f"Loss: {loss_:3.5f}"
+        )
+        logger.info(msg)
+
+        loss.backward()
+
+        lr_psi2 = optimizer.param_groups[0]["lr"]
+        norm_grad_psi2 = psi2.grad.norm().item()
+        torch.nn.utils.clip_grad_norm_([psi2], max_norm=1e-1)
+        norm_grad_psi2_ = psi2.grad.norm().item()
+
+        lr_dpsi2 = optimizer.param_groups[1]["lr"]
+        norm_grad_dpsi2 = dpsi2.grad.norm().item()
+        torch.nn.utils.clip_grad_norm_([dpsi2], max_norm=1e-1)
+        norm_grad_dpsi2_ = dpsi2.grad.norm().item()
+
+        with logger.section("ѱ₂ parameters:", level=logging.DETAIL):
+            msg = f"Learning rate {lr_psi2:.1e}"
+            logger.detail(msg)
+            msg = (
+                f"Gradient norm: {norm_grad_psi2:.1e} -> {norm_grad_psi2_:.1e}"
+            )
+            logger.detail(msg)
+        with logger.section("dѱ₂ parameters:", level=logging.DETAIL):
+            msg = f"Learning rate {lr_dpsi2:.1e}"
+            logger.detail(msg)
+            msg = (
+                f"Gradient norm: {norm_grad_dpsi2:.1e} ->"
+                f" {norm_grad_dpsi2_:.1e}"
+            )
+            logger.detail(msg)
+
+        optimizer.step()
+        scheduler.step(loss)
+
+    best_loss = register_params_dpsi2.best_loss
+    msg = f"ѱ₂ and dѱ₂ optimization completed with loss: {best_loss:3.5f}"
+    logger.info(box(msg, style="round"))
+    output = {
+        "cycle": c,
+        "coords": (imin, imax, jmin, jmax),
+        "psi2": register_params_dpsi2.params["psi2"].detach().cpu(),
+        "dpsi2": register_params_dpsi2.params["dpsi2"].detach().cpu(),
+    }
+    outputs.append(output)
+f = output_dir.joinpath(
+    f"results_psi2_reg_vort_{imin}_{imax}_{jmin}_{jmax}.pt"
+)
+torch.save(outputs, f)
+msg = f"Outputs saved to {f}"
+logger.info(box(msg, style="="))
