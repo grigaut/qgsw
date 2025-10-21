1a8dec5b1e7f52a38c031e9d8e074a54fb386348
grigaut <gaetanrigaut.s@gmail.com>
2025-10-21 10:24:37 +0200

    Correct scripts' names for oar

--- scripts/oar/run_var_analysis_psi2_reg_two_steps.sh
+++ scripts/oar/run_var_analysis_psi2_reg_two_steps.sh
@@ -15,9 +15,9 @@ cmd1="${cmd} --indices 32 96 64 192"
 cmd2="${cmd} --indices 32 96 256 384"
 cmd3="${cmd} --indices 112 176 64 192"
 cmd4="${cmd} --indices 112 176 256 384"
-oarsub -S "$cmd1" -n "VA-psi2-reg-[32 96 64 192]"
-oarsub -S "$cmd2" -n "VA-psi2-reg-[32 96 256 384]"
-oarsub -S "$cmd3" -n "VA-psi2-reg-[112 176 64 192]"
-oarsub -S "$cmd4" -n "VA-psi2-reg-[112 176 256 384]"
+oarsub -S "$cmd1" -n "VA-psi2-rtw-[32 96 64 192]"
+oarsub -S "$cmd2" -n "VA-psi2-rtw-[32 96 256 384]"
+oarsub -S "$cmd3" -n "VA-psi2-rtw-[112 176 64 192]"
+oarsub -S "$cmd4" -n "VA-psi2-rtw-[112 176 256 384]"
 
 exit 1
