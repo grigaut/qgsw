ccb7fcc1ceabc5f61ba83e730f64f15753134ef3
grigaut <gaetanrigaut.s@gmail.com>
2025-12-04 10:13:15 +0100

    Revert using the with_docstring decorator

--- src/qgsw/decomposition/supports/time/gaussian.py
+++ src/qgsw/decomposition/supports/time/gaussian.py
@@ -1,5 +1,6 @@
 """Gaussian time supports."""
 
+# ruff: noqa: D107, D102
 from typing import Any
 
 import torch
@@ -7,13 +8,11 @@ from torch._tensor import Tensor
 
 from qgsw import specs
 from qgsw.decomposition.supports.time.base import TimeSupportFunction
-from qgsw.utils.docstrings import with_docstring
 
 
 class GaussianTimeSupport(TimeSupportFunction):
     """Gaussian time support."""
 
-    @with_docstring(TimeSupportFunction.__init__.__doc__)
     def __init__(
         self,
         time_params: dict[int, dict[str, Any]],
@@ -22,7 +21,8 @@ class GaussianTimeSupport(TimeSupportFunction):
         self._space = space_fields
         self.params = time_params
 
-    @with_docstring(TimeSupportFunction.decompose.__doc__)
+    __init__.__doc__ = TimeSupportFunction.__init__.__doc__
+
     def decompose(self, t: Tensor) -> dict[int, Tensor]:
         fields = {}
         tspecs = specs.from_tensor(t)
@@ -38,16 +38,18 @@ class GaussianTimeSupport(TimeSupportFunction):
             fields[lvl] = torch.einsum("t,txy->xy", exp_, self._space[lvl])
         return fields
 
-    @with_docstring(TimeSupportFunction.__call__.__doc__)
+    decompose.__doc__ = TimeSupportFunction.decompose.__doc__
+
     def __call__(self, t: Tensor) -> Tensor:
         fields = self.decompose(t)
         return sum(fields.values()) / len(self.params)
+
+    __call__.__doc__ = TimeSupportFunction.__call__.__doc__
 
 
 class GaussianTimeSupportDt(GaussianTimeSupport):
     """Time-derivative of Gaussian support function."""
 
-    @with_docstring(TimeSupportFunction.decompose.__doc__)
     def decompose(self, t: Tensor) -> dict[int, Tensor]:
         fields = {}
         tspecs = specs.from_tensor(t)
@@ -66,3 +68,5 @@ class GaussianTimeSupportDt(GaussianTimeSupport):
 
             fields[lvl] = torch.einsum("t,txy->xy", dt_e, space)
         return fields
+
+    decompose.__doc__ = TimeSupportFunction.decompose.__doc__
