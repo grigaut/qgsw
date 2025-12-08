147ec7d8ff68082aff49d958dcf6bbff14f5632a
grigaut <gaetanrigaut.s@gmail.com>
2025-12-08 16:40:30 +0100

    Use einsum in TaylorSeriesTimeSupport

--- src/qgsw/decomposition/supports/time/taylor.py
+++ src/qgsw/decomposition/supports/time/taylor.py
@@ -28,7 +28,9 @@ class TaylorSeriesTimeSupport(TimeSupportFunction):
         for lvl in self.params:
             fact_lvl = factorial(lvl)
 
-            fields[lvl] = t**lvl / fact_lvl * self._space[lvl]
+            fields[lvl] = torch.einsum(
+                "t,txy->xy", t**lvl / fact_lvl, self._space[lvl]
+            )
         return fields
 
     decompose.__doc__ = TimeSupportFunction.decompose.__doc__
@@ -46,7 +48,9 @@ class TaylorSeriesTimeSupport(TimeSupportFunction):
                 fields[lvl] = torch.zeros_like(self._space[lvl])
                 continue
             fact_lvl = factorial(lvl - 1)
-            fields[lvl] = t ** (lvl - 1) / fact_lvl * self._space[lvl]
+            fields[lvl] = torch.einsum(
+                "t,txy->xy", t ** (lvl - 1) / fact_lvl, self._space[lvl]
+            )
         return fields
 
     decompose_dt.__doc__ = TimeSupportFunction.decompose_dt.__doc__
