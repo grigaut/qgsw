2073e0e20a157c81981593a3c50eb67d2e8b1067
grigaut <gaetanrigaut.s@gmail.com>
2025-12-10 17:19:58 +0100

    Allow for slices of the original xx and yy tensors

--- src/qgsw/decomposition/taylor/core.py
+++ src/qgsw/decomposition/taylor/core.py
@@ -1,10 +1,13 @@
 """Core class for Taylor Series time support."""
 
+from typing import Any
+
 import torch
 
 from qgsw.decomposition.base import SpaceTimeDecomposition
 from qgsw.decomposition.coefficients import DecompositionCoefs
 from qgsw.decomposition.supports.space.full_field import FullFieldSpaceSupport
+from qgsw.decomposition.supports.time.base import TimeSupportFunction
 from qgsw.decomposition.supports.time.taylor import TaylorSeriesTimeSupport
 
 
@@ -33,44 +36,62 @@ class TaylorFullFieldBasis(
 
         return DecompositionCoefs.from_dict(coefs)
 
-    numel.__doc__ = SpaceTimeDecomposition[
+    generate_random_coefs.__doc__ = SpaceTimeDecomposition[
         FullFieldSpaceSupport, TaylorSeriesTimeSupport
     ].generate_random_coefs.__doc__
 
-    def _build_space(self) -> dict[int, torch.Tensor]:
+    def _build_space(
+        self, xx: torch.Tensor, yy: torch.Tensor
+    ) -> dict[int, torch.Tensor]:
         fields = {}
+
+        nx, ny = xx.shape[-2:]
 
         for lvl, c in self._coefs.items():
             params = self._space[lvl]
-            nx: float = params["nx"]
-            ny: float = params["ny"]
+            Nx, Ny = params["xs"].shape[0], params["ys"].shape[0]
+            mask = self._compute_mask(params, xx, yy)[None, ...]
 
             full_field = FullFieldSpaceSupport(nx, ny, **self._specs)
+            coefs = c.reshape((-1, Nx, Ny))[mask].reshape((-1, nx, ny))
             fields[lvl] = torch.einsum(
                 "txy,cxy->txy",
-                c.reshape((-1, nx, ny)),
+                coefs,
                 full_field.field,
             )
         return fields
 
-    def _check_locations(self, xx: torch.Tensor, yy: torch.Tensor) -> None:
-        nx = self._space[0]["nx"]
-        ny = self._space[0]["ny"]
-        if (s := xx.shape) != (nx, ny):
-            msg = f"X-locations must be {nx} x {ny} shaped, not {s}."
+    def _compute_mask(
+        self, params: dict[str, Any], xx: torch.Tensor, yy: torch.Tensor
+    ) -> torch.Tensor:
+        xs = params["xs"]
+        ys = params["ys"]
+
+        x_idx = torch.searchsorted(xs, xx)
+        y_idx = torch.searchsorted(ys, yy)
+        if not torch.all(xs[x_idx] == xx):
+            msg = "x contains values that do not match xs."
             raise ValueError(msg)
-        if (s := yy.shape) != (nx, ny):
-            msg = f"y-locations must be {nx} x {ny} shaped, not {s}."
+        if not torch.all(ys[y_idx] == yy):
+            msg = "y contains values that do not match ys."
             raise ValueError(msg)
 
-    def localize(  # noqa: D102
-        self, xx: torch.Tensor, yy: torch.Tensor
-    ) -> TaylorSeriesTimeSupport:
-        self._check_locations(xx, yy)
-        space_fields = self._build_space()
+        mask = torch.zeros(
+            (xs.shape[0], ys.shape[0]),
+            dtype=torch.bool,
+            device=self._specs["device"],
+        )
+        mask[x_idx, y_idx] = True
 
-        return TaylorSeriesTimeSupport(self._time, space_fields)
+        return mask
 
-    localize.__doc__ = SpaceTimeDecomposition[
+    def generate_time_support(  # noqa: D102
+        self,
+        time_params: dict[int, dict[str, Any]],
+        space_fields: dict[int, torch.Tensor],
+    ) -> TimeSupportFunction:
+        return TaylorSeriesTimeSupport(time_params, space_fields)
+
+    generate_time_support.__doc__ = SpaceTimeDecomposition[
         FullFieldSpaceSupport, TaylorSeriesTimeSupport
-    ].localize.__doc__
+    ].generate_time_support.__doc__
