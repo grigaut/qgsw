28eb66c900bf7775a641cae4579bd3d0a5f20c8d
grigaut <gaetanrigaut.s@gmail.com>
2026-02-19 18:42:10 +0100

    Implement different calculations for stream funciton

--- src/qgsw/eNATL60/fields_computations.py
+++ src/qgsw/eNATL60/fields_computations.py
@@ -3,25 +3,67 @@
 import xarray as xr
 
 from qgsw.eNATL60.forcing import interpolate_era_da
-from qgsw.eNATL60.var_keys import MEAN_SEA_LEVEL, SSH
+from qgsw.eNATL60.var_keys import (
+    ATMOS_PRESSURE,
+    LATITUDE,
+    LONGITUDE,
+    SSH,
+)
 
 
-def compute_streamfunction(
+def compute_streamfunction_with_atmospheric_pressure(
     ds: xr.Dataset,
     ds_era: xr.Dataset,
     rho0: float = 1026.0,
     g: float = 9.81,
+    *,
+    remove_avgs: bool = False,
 ) -> xr.DataArray:
     """Compute surface streamfunction.
 
     Args:
-        ds (xr.Dataset): _description_
-        ds_era (xr.Dataset): _description_
-        rho0 (float, optional): _description_. Defaults to 1026.0.
-        g (float, optional): _description_. Defaults to 9.81.
+        ds (xr.Dataset): Sea surface height.
+        ds_era (xr.Dataset): Atmospheric datas.
+        rho0 (float, optional): Sea density. Defaults to 1026.0.
+        g (float, optional): Gravity constant. Defaults to 9.81.
+        remove_avgs (bool, optional): Whether to remove atmospheric pressure
+            average (spatial average evaluated at every time) and ssh average
+            (both in time and space) or not. Defauts to False
 
     Returns:
         xr.DataArray: _description_
     """
-    msl = interpolate_era_da(ds_era[MEAN_SEA_LEVEL], ds)
-    return msl / rho0 + g * ds[SSH]
+    atmos_p = interpolate_era_da(ds_era[ATMOS_PRESSURE], ds)
+    ssh = ds[SSH]
+    if remove_avgs:
+        atmos_p_avg = atmos_p.mean(dim=[LONGITUDE, LATITUDE])
+        atmos_p = atmos_p - atmos_p_avg
+        ssh_avg = ssh.mean()
+        ssh = ssh - ssh_avg
+    return atmos_p / rho0 + g * ssh
+
+
+def compute_stream_function_ssh_only(
+    ds: xr.Dataset,
+    g: float = 9.81,
+    *,
+    remove_avg: bool = False,
+) -> xr.DataArray:
+    """Compute surface streamfunction.
+
+    Args:
+        ds (xr.Dataset): Sea surface height.
+        ds_era (xr.Dataset): Atmospheric datas.
+        rho0 (float, optional): Sea density. Defaults to 1026.0.
+        g (float, optional): Gravity constant. Defaults to 9.81.
+        remove_avg (bool, optional): Whether to remove ssh average or not.
+            Defauts to False
+
+    Returns:
+        xr.DataArray: _description_
+    """
+    ssh = ds[SSH]
+    if remove_avg:
+        ssh_avg = ssh.mean()
+        ssh = ssh - ssh_avg
+    return g * ssh
