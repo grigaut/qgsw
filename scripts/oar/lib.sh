291eaa360fd72a0692d006839847a10d4846ef47
grigaut <gaetanrigaut.s@gmail.com>
2026-03-06 16:37:58 +0100

    Correct host names

--- scripts/oar/lib.sh
+++ scripts/oar/lib.sh
@@ -8,14 +8,7 @@ load_env() {
         set +a
     fi
 }
-oarnodes --sql "production='YES' and state='Alive'" -J | python3 -c "
-import json, sys
-nodes = json.load(sys.stdin)
-gpu_nodes = [info for info in nodes.values() if info.get('gpu') is not None]
-sorted_nodes = sorted(gpu_nodes, key=lambda x: x.get('memnode') or 0, reverse=True)
-for info in sorted_nodes[:10]:
-    print(f\"{info.get('host', 'N/A')}: {info.get('memnode', 'N/A')} MB RAM, gpu={info.get('gpu')}, gpu_model={info.get('gpu_model', 'N/A')}\")
-"
+
 build_oar_opts() {
     local walltime=$1
     OAR_OPTS=(
@@ -32,7 +25,7 @@ build_oar_opts() {
 build_oar_opts_hr() {
     local walltime=$1
     OAR_OPTS=(
-        -p "host='abacus1' OR host='abacus11'"
+        -p "host='abacus11'"
         -q production
         -l "gpu=1,walltime=${walltime}"
         -O logs/OAR.%jobid%.stdout
