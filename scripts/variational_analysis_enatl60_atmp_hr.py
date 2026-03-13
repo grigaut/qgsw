Correct loss computation

scripts/variational_analysis_enatl60_atmp_hr.py
@@ -244,14 +244,16 @@ def update_loss(
     f: torch.Tensor,
     f_ref: torch.Tensor,
     time: torch.Tensor,
-) -> torch.Tensor:
+    *,
+    variance: float | torch.Tensor = 1,
+) -> None:
     """Update loss."""
     mask = obs_mask.at_time(time)
     if not mask.any():
         return loss
     f_sliced = f.flatten()[mask.flatten()]
     f_ref_sliced = f_ref.flatten()[mask.flatten()]
-    return loss + mse(f_sliced, f_ref_sliced)
+    return loss + mse(f_sliced, f_ref_sliced) / variance
 
 
 ## Regularization
@@ -322,9 +324,9 @@ slip_coef = config.physics.slip_coef
 ## Error
 
 
-def mse(f: torch.Tensor, f_ref: torch.Tensor) -> float:
+def mse(f: torch.Tensor, f_ref: torch.Tensor) -> torch.Tensor:
     """RMSE."""
-    return (f - f_ref).square().mean() / f_ref.square().mean()
+    return (f - f_ref).square().mean()
 
 
 ## Bulk formula (from Large & Yeager 2004)
@@ -559,6 +561,8 @@ for c in range(n_cycles):
         for p in ds_interp[STREAMFUNCTION].to_numpy()
     ]
 
+    var_ref = torch.stack([crop(psi[0, 0], b) for psi in psis_ref]).var()
+
     with logger.timeit("Computing psi boundaries"):
         psis_filt = [
             torch.tensor(p, **specs).unsqueeze(0).unsqueeze(0) / beta_plane.f0
@@ -733,6 +737,7 @@ for c in range(n_cycles):
                 model.psi[0, 0],
                 crop(psis_ref[0][0, 0], b),
                 model.time,
+                variance=var_ref,
             )
 
             for n in range(1, n_steps_per_cyle):

scripts/variational_analysis_forced_rg_dr_perturbed.py
@@ -131,6 +131,8 @@ def update_loss(
     f: torch.Tensor,
     f_ref: torch.Tensor,
     time: torch.Tensor,
+    *,
+    variance: float | torch.Tensor = 1,
 ) -> None:
     """Update loss."""
     mask = obs_mask.at_time(time)
@@ -138,7 +140,7 @@ def update_loss(
         return loss
     f_sliced = f.flatten()[mask.flatten()]
     f_ref_sliced = f_ref.flatten()[mask.flatten()]
-    return loss + mse(f_sliced, f_ref_sliced)
+    return loss + mse(f_sliced, f_ref_sliced) / variance
 
 
 ## Regularization
@@ -213,9 +215,9 @@ psi_start = P.compute_p(covphys.to_cov(uvh0, dx, dy))[0] / beta_plane.f0
 ## Error
 
 
-def mse(f: torch.Tensor, f_ref: torch.Tensor) -> float:
+def mse(f: torch.Tensor, f_ref: torch.Tensor) -> torch.Tensor:
     """RMSE."""
-    return (f - f_ref).square().mean() / f_ref.square().mean()
+    return (f - f_ref).square().mean()
 
 
 # Models
@@ -340,6 +342,8 @@ for c in range(n_cycles):
     msg = f"Cycle {step(c + 1, n_cycles)}: Model spin-up completed."
     logger.info(box(msg, style="round"))
 
+    var_ref = torch.stack([crop(psi[0, 0], p) for psi in psis]).var()
+
     space_params, time_params = dyadic_decomposition(
         order=5,
         xx_ref=space_slice.psi.xy.x,
@@ -415,6 +419,7 @@ for c in range(n_cycles):
                     model.psi[0, 0],
                     crop(psis[n][0, 0], p),
                     model.time,
+                    variance=var_ref,
                 )
             if with_reg:
                 for coef in coefs.values():

scripts/variational_analysis_psi2.py
@@ -9,6 +9,7 @@ import torch
 
 from qgsw.cli import ScriptArgsVAModified
 from qgsw.configs.core import Configuration
+from qgsw.decomposition.coefficients import DecompositionCoefs
 from qgsw.decomposition.exp_exp.core import GaussianExpBasis
 from qgsw.decomposition.exp_exp.param_generator import gaussian_exp_field
 from qgsw.fields.variables.tuples import UVH
@@ -22,7 +23,6 @@ from qgsw.models.qg.uvh.projectors.core import QGProjector
 from qgsw.observations import FullDomainMask, SatelliteTrackMask
 from qgsw.optim.callbacks import LRChangeCallback
 from qgsw.optim.utils import EarlyStop, RegisterParams
-from qgsw.solver.boundary_conditions.base import Boundaries
 from qgsw.spatial.core.discretization import (
     SpaceDiscretization3D,
 )
@@ -122,6 +122,8 @@ def update_loss(
     f: torch.Tensor,
     f_ref: torch.Tensor,
     time: torch.Tensor,
+    *,
+    variance: float | torch.Tensor = 1,
 ) -> None:
     """Update loss."""
     mask = obs_mask.at_time(time)
@@ -129,7 +131,7 @@ def update_loss(
         return loss
     f_sliced = f.flatten()[mask.flatten()]
     f_ref_sliced = f_ref.flatten()[mask.flatten()]
-    return loss + mse(f_sliced, f_ref_sliced)
+    return loss + mse(f_sliced, f_ref_sliced) / variance
 
 
 ## Regularization
@@ -206,9 +208,9 @@ psi_start = P.compute_p(covphys.to_cov(uvh0, dx, dy))[0] / beta_plane.f0
 ## Error
 
 
-def mse(f: torch.Tensor, f_ref: torch.Tensor) -> float:
+def mse(f: torch.Tensor, f_ref: torch.Tensor) -> torch.Tensor:
     """RMSE."""
-    return (f - f_ref).square().mean() / f_ref.square().mean()
+    return (f - f_ref).square().mean()
 
 
 # Models
@@ -262,11 +264,6 @@ def extract_psi_w(psi: torch.Tensor) -> torch.Tensor:
     return psi[..., psi_slices_w[0], psi_slices_w[1]]
 
 
-def extract_psi_bc(psi: torch.Tensor) -> Boundaries:
-    """Extract psi."""
-    return Boundaries.extract(psi, p, -p - 1, p, -p - 1, 2)
-
-
 for c in range(n_cycles):
     torch.cuda.reset_peak_memory_stats()
     times = [model_3l.time.clone()]
@@ -281,21 +278,22 @@ for c in range(n_cycles):
         times.append(model_3l.time.clone())
 
         psi = extract_psi_w(model_3l.psi[:, :2])
-        psi_bc = extract_psi_bc(psi)
 
         psis.append(psi)
 
     msg = f"Cycle {step(c + 1, n_cycles)}: Model spin-up completed."
     logger.info(box(msg, style="round"))
 
-    xx = space_slice_ww.psi.xy.x
-    yy = space_slice_ww.psi.xy.y
+    var_ref = torch.stack([crop(psi[0, 0], p) for psi in psis]).var()
+
+    xx = space_slice.psi.xy.x
+    yy = space_slice.psi.xy.y
 
     space_params, time_params = gaussian_exp_field(
         0, 3, xx, yy, n_steps_per_cyle * dt, n_steps_per_cyle / 6 * 7200
     )
     basis = GaussianExpBasis(space_params, time_params)
-    coefs = basis.generate_random_coefs()
+    coefs = DecompositionCoefs.zeros_like(basis.generate_random_coefs())
     coefs = coefs.requires_grad_()
 
     if with_alpha:
@@ -357,7 +355,7 @@ for c in range(n_cycles):
 
                 loss = update_loss(
                     loss,
-                    crop(psi2, p),
+                    psi2,
                     crop(psis[n][0, 1], p),
                     times[n] - times[0],
                 )

scripts/variational_analysis_surfml_perturbed.py
@@ -140,6 +140,8 @@ def update_loss(
     f: torch.Tensor,
     f_ref: torch.Tensor,
     time: torch.Tensor,
+    *,
+    variance: float | torch.Tensor = 1,
 ) -> None:
     """Update loss."""
     mask = obs_mask.at_time(time)
@@ -147,7 +149,7 @@ def update_loss(
         return loss
     f_sliced = f.flatten()[mask.flatten()]
     f_ref_sliced = f_ref.flatten()[mask.flatten()]
-    return loss + mse(f_sliced, f_ref_sliced)
+    return loss + mse(f_sliced, f_ref_sliced) / variance
 
 
 ## Regularization
@@ -222,9 +224,9 @@ psi_start = P.compute_p(covphys.to_cov(uvh0, dx, dy))[0] / beta_plane.f0
 ## Error
 
 
-def mse(f: torch.Tensor, f_ref: torch.Tensor) -> float:
+def mse(f: torch.Tensor, f_ref: torch.Tensor) -> torch.Tensor:
     """RMSE."""
-    return (f - f_ref).square().mean() / f_ref.square().mean()
+    return (f - f_ref).square().mean()
 
 
 # Models
@@ -423,6 +425,8 @@ for c in range(n_cycles):
     msg = f"Cycle {step(c + 1, n_cycles)}: Model spin-up completed."
     logger.info(box(msg, style="round"))
 
+    var_ref = torch.stack([crop(psi[0, 0], p) for psi in psis]).var()
+
     psi_bc_interp = QuadraticInterpolation(times, psi_bcs)
 
     xx = space_slice.psi.xy.x
@@ -537,6 +541,7 @@ for c in range(n_cycles):
                     psi1[0, 0],
                     crop(psis[n][0, 0], p),
                     model.time,
+                    variance=var_ref,
                 )
 
         if torch.isnan(loss.detach()):