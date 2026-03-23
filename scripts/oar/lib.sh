52855c877565c9c74a6e9b66c06ac351fa01e5b2
grigaut <gaetanrigaut.s@gmail.com>
2026-03-23 16:17:17 +0100

    Allow for more options

--- scripts/oar/lib.sh
+++ scripts/oar/lib.sh
@@ -39,12 +39,27 @@ build_oar_opts_hr() {
 parse_common_flags() {
     contiguous=false
     long=false
+    long_optim=false
+    any_zone=false
+    z1=false
+    z2=false
+    z3=false
+    z4=false
     args=()
     for arg in "$@"; do
         case "$arg" in
-            --contiguous) contiguous=true ;;
-            --long)       long=true ;;
-            *)            args+=("$arg") ;;
+            --contiguous)  contiguous=true ;;
+            --long)        long=true ;;
+            --long-optim)  long_optim=true ;;
+            --z1)          z1=true; any_zone=true ;;
+            --z2)          z2=true; any_zone=true ;;
+            --z3)          z3=true; any_zone=true ;;
+            --z4)          z4=true; any_zone=true ;;
+            *)             args+=("$arg") ;;
         esac
     done
+    # If no zone specified, enable all
+    if [ "$any_zone" = false ]; then
+        z1=true; z2=true; z3=true; z4=true
+    fi
 }
--- scripts/oar/run_var_analysis_forced_rg_dr.sh
+++ scripts/oar/run_var_analysis_forced_rg_dr.sh
@@ -10,16 +10,19 @@ chmod +x $SCRIPT
 parse_common_flags "$@"
 load_env "$SRCDIR"
 
-# Set walltime based on --long and --contiguous flags
-if [ "$long" = true ] && [ "$contiguous" = true ]; then
-    walltime=60
-elif [ "$contiguous" = true ]; then
-    walltime=16
-elif [ "$long" = true ]; then
-    walltime=16
-else
-    walltime=4
-fi
+# Count active zones
+n_zones=0
+[ "$z1" = true ] && (( n_zones++ ))
+[ "$z2" = true ] && (( n_zones++ ))
+[ "$z3" = true ] && (( n_zones++ ))
+[ "$z4" = true ] && (( n_zones++ ))
+
+# Compute walltime
+walltime=4
+[ "$long_optim" = true ] && (( walltime *= 4 ))
+[ "$long" = true ]       && (( walltime *= 4 ))
+[ "$contiguous" = true ] && (( walltime *= n_zones ))
+
 build_oar_opts "$walltime"
 
 # Build base command with filtered arguments
@@ -30,8 +33,11 @@ done
 
 # Append extra python args based on flags
 optim_args=""
-if [ "$long" = true ]; then
+if [ "$long_optim" = true ]; then
     optim_args+=" -o 800"
+fi
+if [ "$long" = true ]; then
+    optim_args+=" -c 12"
 fi
 
 # Build the four command variants
@@ -41,13 +47,19 @@ cmd3="${cmd}${optim_args} --indices 112 176 64 192"
 cmd4="${cmd}${optim_args} --indices 112 176 256 384"
 
 if [ "$contiguous" = true ]; then
-    combined_cmd="$cmd1 ; $cmd2 ; $cmd3 ; $cmd4"
+    combined_cmd=""
+    [ "$z1" = true ] && combined_cmd+="${cmd1} ; "
+    [ "$z2" = true ] && combined_cmd+="${cmd2} ; "
+    [ "$z3" = true ] && combined_cmd+="${cmd3} ; "
+    [ "$z4" = true ] && combined_cmd+="${cmd4} ; "
+    # Strip trailing " ; "
+    combined_cmd="${combined_cmd% ; }"
     oarsub "${OAR_OPTS[@]}" -n "${NAME}-contiguous" "$combined_cmd"
 else
-    oarsub "${OAR_OPTS[@]}" -n "${NAME}-[32 96 64 192]"    "$cmd1"
-    oarsub "${OAR_OPTS[@]}" -n "${NAME}-[32 96 256 384]"   "$cmd2"
-    oarsub "${OAR_OPTS[@]}" -n "${NAME}-[112 176 64 192]"  "$cmd3"
-    oarsub "${OAR_OPTS[@]}" -n "${NAME}-[112 176 256 384]" "$cmd4"
+    [ "$z1" = true ] && oarsub "${OAR_OPTS[@]}" -n "${NAME}-[32 96 64 192]"    "$cmd1"
+    [ "$z2" = true ] && oarsub "${OAR_OPTS[@]}" -n "${NAME}-[32 96 256 384]"   "$cmd2"
+    [ "$z3" = true ] && oarsub "${OAR_OPTS[@]}" -n "${NAME}-[112 176 64 192]"  "$cmd3"
+    [ "$z4" = true ] && oarsub "${OAR_OPTS[@]}" -n "${NAME}-[112 176 256 384]" "$cmd4"
 fi
 
 exit 0
