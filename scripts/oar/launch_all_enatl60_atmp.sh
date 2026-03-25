db0c4261c95e810059d05b4ba5cf39c8da7c776c
grigaut <gaetanrigaut.s@gmail.com>
2026-03-25 14:53:41 +0100

    Modify default model params

--- scripts/oar/launch_all_enatl60_atmp.sh
+++ scripts/oar/launch_all_enatl60_atmp.sh
@@ -4,22 +4,22 @@ cd $SRCDIR
 
 # SurfML
 
-./scripts/oar/run_var_analysis_enatl60_atmp.sh --config=config/variational_analysis.toml -v --obs-track --gamma=0.1 "$@"
+./scripts/oar/run_var_analysis_enatl60_atmp.sh --config=config/enatl60.toml -v --obs-track --gamma=0.1 "$@"
 
-./scripts/oar/run_var_analysis_enatl60_atmp.sh --config=config/variational_analysis.toml -v --obs-track --gamma=0.1 --no-alpha "$@"
+./scripts/oar/run_var_analysis_enatl60_atmp.sh --config=config/enatl60.toml -v --obs-track --gamma=0.1 --no-alpha "$@"
 
-./scripts/oar/run_var_analysis_enatl60_atmp.sh --config=config/variational_analysis.toml -v --obs-track --no-reg "$@"
+./scripts/oar/run_var_analysis_enatl60_atmp.sh --config=config/enatl60.toml -v --obs-track --no-reg "$@"
 
-./scripts/oar/run_var_analysis_enatl60_atmp.sh --config=config/variational_analysis.toml -v --obs-track --no-reg --no-alpha "$@"
+./scripts/oar/run_var_analysis_enatl60_atmp.sh --config=config/enatl60.toml -v --obs-track --no-reg --no-alpha "$@"
 
 # Forced
 
-./scripts/oar/run_var_analysis_enatl60_forced_atmp.sh --config=config/variational_analysis.toml -v --obs-track --gamma=1e4 "$@"
+./scripts/oar/run_var_analysis_enatl60_forced_atmp.sh --config=config/enatl60.toml -v --obs-track --gamma=1e4 "$@"
 
-./scripts/oar/run_var_analysis_enatl60_forced_atmp.sh --config=config/variational_analysis.toml -v --obs-track --no-reg "$@"
+./scripts/oar/run_var_analysis_enatl60_forced_atmp.sh --config=config/enatl60.toml -v --obs-track --no-reg "$@"
 
 # Forced + SurfML
 
-./scripts/oar/run_var_analysis_enatl60_forcedsurf_atmp.sh --config=config/variational_analysis.toml -v --obs-track --gamma=1 "$@"
+./scripts/oar/run_var_analysis_enatl60_forcedsurf_atmp.sh --config=config/enatl60.toml -v --obs-track --gamma=1 "$@"
 
-./scripts/oar/run_var_analysis_enatl60_forcedsurf_atmp.sh --config=config/variational_analysis.toml -v --obs-track --no-reg "$@"
+./scripts/oar/run_var_analysis_enatl60_forcedsurf_atmp.sh --config=config/enatl60.toml -v --obs-track --no-reg "$@"
