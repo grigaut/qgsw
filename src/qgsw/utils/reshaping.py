f123383e9c92db62477b8954001a7eb6d2a43f70
grigaut <gaetanrigaut.s@gmail.com>
2026-08-20 09:55:52 +0200

    Support typing for np arrays in crop

--- src/qgsw/utils/reshaping.py
+++ src/qgsw/utils/reshaping.py
@@ -1,17 +1,33 @@
 """Reshaping utils."""
 
-import torch
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, overload
+
+if TYPE_CHECKING:
+    import numpy as np
+    import torch
 
 
-def crop(t: torch.Tensor, offset: int = 0) -> torch.Tensor:
+@overload
+def crop(t: torch.Tensor, offset: int) -> torch.Tensor: ...
+
+
+@overload
+def crop(t: np.ndarray, offset: int) -> np.ndarray: ...
+
+
+def crop(
+    t: torch.Tensor | np.ndarray, offset: int = 0
+) -> torch.Tensor | np.ndarray:
     """Crop the last two dimensions of a tensor.
 
     Args:
-        t (torch.Tensor): Tensor to crop.
+        t (torch.Tensor | np.ndarray): Array / Tensor to crop.
         offset (int, optional): Crop width. Defaults to 0.
 
     Returns:
-        torch.Tensor: Cropped tensor.
+        torch.Tensor | np.ndarray: Cropped array / tensor.
     """
     if offset < 0:
         msg = "Cropping offset must be greater or equal to 0."
