b4a8e00a602f93a1f747eb603b137764d2e24b7c
grigaut <gaetanrigaut.s@gmail.com>
2026-09-01 14:12:48 +0200

    FIX: rewrite eval_loss function

--- src/qgsw/scripts/loss.py
+++ src/qgsw/scripts/loss.py
@@ -4,6 +4,25 @@ from __future__ import annotations
 
 import torch
 
+from qgsw import specs
+
+
+def eval_loss(
+    f: torch.Tensor,
+    f_ref: torch.Tensor,
+    *,
+    mask: torch.Tensor | None = None,
+    variance: float | torch.Tensor = 1,
+) -> torch.Tensor:
+    """Update loss."""
+    if mask is None:
+        mask = torch.ones_like(f_ref, dtype=torch.bool)
+    if not mask.any():
+        return torch.tensor(0, **specs.from_tensor(f))
+    f_sliced = f.flatten()[mask.flatten()]
+    f_ref_sliced = f_ref.flatten()[mask.flatten()]
+    return (f_sliced - f_ref_sliced).square().sum() / variance
+
 
 def update_loss(
     loss: torch.Tensor,
