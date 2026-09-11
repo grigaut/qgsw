1cec9e2ab64d6977a2b32cd84a09ac2afb261567
grigaut <gaetanrigaut.s@gmail.com>
2026-09-11 16:32:05 +0200

    Create script to launch all sst models

---
+++ scripts/oar/launch_all_enatl60_sst.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+SRCDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
+cd $SRCDIR
+
+# RGSI SST
+
+./scripts/oar/run_var_analysis_enatl60_sst_hr.sh --config=config/enatl60.toml -v --obs-track --gamma=0.01 --gamma-sst=0.01 "$@"
+
+# RGSI SST Adv
+
+./scripts/oar/run_var_analysis_enatl60_sst_adv_hr.sh --config=config/enatl60.toml -v --obs-track --gamma=0.01 --gamma-sst=0.01 "$@"
+
+# Forced SST Adv
+
+./scripts/oar/run_var_analysis_enatl60_forced_sst_hr.sh --config=config/enatl60.toml -v --obs-track --gamma=1e3 --gamma-sst=0.01 "$@"
